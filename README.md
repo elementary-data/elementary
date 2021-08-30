c1dd442712c22ff49bd8491264e2ae309def2506
oravidov <or@singular.net>
2021-08-30 19:31:35 +0300

    Added setup files

---
+++ README.md
@@ -0,0 +1,27 @@
+# Elementary Data Lineage
+A simple commandline to present data lineage based on your data warehourse query history
+# Installation
+## Using Pip
+```bash
+  $ pip install elementary-lineage
+```
+## Manual
+```bash
+  $ git clone https://github.com/oravi/lineage.git
+  $ cd lineage
+  $ python setup.py install
+```
+# Usage
+```bash
+$ lineage --help
+```
+## Using a configuration file
+`search <keyword>`
+```bash
+$ lineage --start-date 2021-08-29 --end-date 2021-08-30 -c ~/lineage_configuration.yml
+```
+## Using dbt profiles.yml
+
+```bash
+$ lineage --start-date 2021-08-29 --end-date 2021-08-30 --dbt-profiles-dir ~/.dbt --dbt-profile-name my_snowflake_profile
+```
---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+from io import open
+from os import path
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+with open(path.join(HERE, 'requirements.txt'), encoding='utf-8') as f:
+    package_requirements = f.read().split('\n')
+
+install_requirements = [x.strip() for x in package_requirements if ('git+' not in x) and (not x.startswith('#')) and
+                        (not x.startswith('-'))]
+
+dependency_links = [x.strip().replace('git+', '') for x in package_requirements if 'git+' not in x]
+
+setup (
+    name='lineage',
+    description='Presenting data lineage based on your data warehourse query history',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requirements,
+    python_requires='>=3.6.2',
+    entry_points='''
+        [console_scripts]
+        lineage=lineage.main:main
+    ''',
+    author="Elementary",
+    keyword="data, lineage, data lineage, data warehourse, DWH",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license='',
+    url='https://github.com/TODO',
+    download_url='https://github.com/TODO',
+    dependency_links=dependency_links,
+    author_email='or@elementary-data.com',
+    classifiers=[
+        #"License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
