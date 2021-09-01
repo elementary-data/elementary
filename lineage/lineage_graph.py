2af716593437ab0eb457c4b1b41ed52d2dd6e473
oravidov <or@singular.net>
2021-09-01 16:05:47 +0300

    Refactored the graph creation to use sqllineage lib

--- lineage/lineage_graph.py
+++ lineage/lineage_graph.py
@@ -1,57 +1,103 @@
+import itertools
+
 import networkx as nx
-from sqllineage import runner
+import sqlparse
+from sqlparse.sql import Statement
+from sqllineage.core import LineageAnalyzer, LineageResult
+from sqllineage.exceptions import SQLLineageException
 from pyvis.network import Network
 import webbrowser
 
-# TODO: add types like a real pro
 # TODO: add logger and debug logs like a pro
 # TODO: add relevant requirements
 
 
 class LineageGraph(object):
-    def __init__(self):
-        self.lineage_graph = nx.DiGraph()
+    def __init__(self, show_islands: bool = False) -> None:
+        self._lineage_graph = nx.DiGraph()
+        self._show_islands = show_islands
 
     @staticmethod
-    def _parse_query(query):
-        try:
-            # Very basic and naive parsing of source and target tables from query
-            lineage_parsed_query = runner.LineageRunner(query)
-            source_tables = lineage_parsed_query.source_tables
-            target_tables = lineage_parsed_query.target_tables
-        except Exception as e:
-            return [], []
+    def _parse_query(query: str) -> [LineageResult]:
+        parsed_query = sqlparse.parse(query.strip())
+        analyzed_statements = [LineageAnalyzer().analyze(statement) for statement in parsed_query
+                               if statement.token_first(skip_cm=True, skip_ws=True)]
+        return analyzed_statements
 
-        return source_tables, target_tables
+    def _update_lineage_graph(self, analyzed_statements: [LineageResult]) -> None:
+        for analyzed_statement in analyzed_statements:
+            # Handle drop tables, if they exist in the statement
+            dropped_tables = analyzed_statement.drop
+            for dropped_table in dropped_tables:
+                self._remove_node(str(dropped_table))
 
-    def init_graph_from_query_list(self, queries, include_islands=False):
+            # Handle rename tables
+            renamed_tables = analyzed_statement.rename
+            for old_table, new_table in renamed_tables:
+                self._rename_node(str(old_table), str(new_table))
+
+            # sqllineage lib marks CTEs as intermediate tables. Remove CTEs (WITH statements) from the source tables.
+            sources = {str(source) for source in analyzed_statement.read - analyzed_statement.intermediate}
+            targets = {str(target) for target in analyzed_statement.write}
+
+            self._add_nodes_and_edges(sources, targets)
+
+    def _add_nodes_and_edges(self, sources: {str}, targets: {str}) -> None:
+        if not sources and not targets:
+            return
+
+        if len(sources) > 0 and len(targets) == 0:
+            if self._show_islands:
+                self._lineage_graph.add_nodes_from(sources)
+        elif len(targets) > 0 and len(sources) == 0:
+            if self._show_islands:
+                self._lineage_graph.add_nodes_from(targets)
+        else:
+            self._lineage_graph.add_nodes_from(sources)
+            self._lineage_graph.add_nodes_from(targets)
+            for source, target in itertools.product(sources, targets):
+                self._lineage_graph.add_edge(source, target)
+
+    def _rename_node(self, old_node: str, new_node: str) -> None:
+        # TODO: consider making this static
+        if self._lineage_graph.has_node(old_node):
+            # Rename in place instead of copying the entire lineage graph
+            nx.relabel_nodes(self._lineage_graph, {old_node: new_node}, copy=False)
+
+    def _remove_node(self, node: str) -> None:
+        # TODO: consider making this static
+        # First let's check if the node exists in the graph
+        if self._lineage_graph.has_node(node):
+            node_successors = self._lineage_graph.successors(node)
+            node_predecessors = self._lineage_graph.predecessors(node)
+
+            # networknx's remove_node already takes care of in and out edges
+            self._lineage_graph.remove_node(node)
+
+            # Now that we have just deleted the dropped table from the graph, we need to take care of
+            # new island nodes.
+            if not self._show_islands:
+                for successor in node_successors:
+                    if self._lineage_graph.degree(successor) == 0:
+                        self._lineage_graph.remove_node(successor)
+                for predecessor in node_predecessors:
+                    if self._lineage_graph.degree(predecessor) == 0:
+                        self._lineage_graph.remove_node(predecessor)
+
+    def init_graph_from_query_list(self, queries: [str]) -> None:
         for query in queries:
-            source_tables, target_tables = self._parse_query(query)
-            # if there is no source, add the target as a node
-            if len(source_tables) == 0 and len(target_tables) > 0:
-                if not include_islands:
-                    continue
-                for target_table in target_tables:
-                    self.lineage_graph.add_node(str(target_table))
-            # if there is no target, add the source as a node
-            elif len(source_tables) > 0 and len(target_tables) == 0:
-                if not include_islands:
-                    continue
-                for source_table in source_tables:
-                    self.lineage_graph.add_node(str(source_table))
-            else:
-                # If both source and target exist, add a new edge to the graph
-                for source in source_tables:
-                    for target in target_tables:
-                        print("Adding edge", str(source), str(target))
-                        self.lineage_graph.add_node(str(source), shape='box')
-                        self.lineage_graph.add_node(str(target), shape='box')
-                        self.lineage_graph.add_edge(str(source), str(target))
+            try:
+                analyzed_statements = self._parse_query(query)
+            except SQLLineageException as e:
+                #TODO: log exception here
+                continue
 
-    def draw_graph(self, should_open_browser=True):
+            self._update_lineage_graph(analyzed_statements)
+
+    def draw_graph(self, should_open_browser: bool = True) -> None:
         # Visualize the graph
         net = Network(height="100%", width="100%", directed=True, notebook=True)
-        net.from_nx(self.lineage_graph)
+        net.from_nx(self._lineage_graph)
         net.set_options("""{
             "edges": {
                 "color": {
--- lineage/query_history.py
+++ lineage/query_history.py
@@ -1,21 +1,40 @@
 from datetime import timedelta
+from lineage.utils import is_flight_mode_on
+import json
+import os
 
 # TODO: check timezone (to_timestamp_ltz), validate escaping
+# TODO: filter only on success queries
 QUERY_HISTORY = """
 select query_text
   from table(elementary_db.information_schema.query_history(
     end_time_range_start=>to_timestamp_ltz('{query_start_time}'),
-    end_time_range_end=>to_timestamp_ltz('{query_end_time}')));
+    end_time_range_end=>to_timestamp_ltz('{query_end_time}'))) 
+    where execution_status = 'SUCCESS'
+    order by end_time;
 """
 
+LATEST_QUERY_HISTORY_FILE = './latest_query_history.json'
 
-def extract_quries_from_query_history(con, start_date, end_date):
+
+def extract_queries_from_query_history(con, start_date, end_date, should_serialize_query_history=True):
+
     query_end_time = end_date + timedelta(hours=23, minutes=59, seconds=59)
     # Load recent queries from history log
     queries = []
-    with con.cursor() as cursor:
-        cursor.execute(QUERY_HISTORY.format(query_start_time=start_date, query_end_time=query_end_time))
-        rows = cursor.fetchall()
-        for row in rows:
-            queries.append(row[0])
+
+    if is_flight_mode_on() and os.path.exists(LATEST_QUERY_HISTORY_FILE):
+        with open(LATEST_QUERY_HISTORY_FILE, 'r') as query_history_file:
+            queries = json.load(query_history_file)
+    else:
+        with con.cursor() as cursor:
+            cursor.execute(QUERY_HISTORY.format(query_start_time=start_date, query_end_time=query_end_time))
+            rows = cursor.fetchall()
+            for row in rows:
+                queries.append(row[0])
+
+        if should_serialize_query_history:
+            with open(LATEST_QUERY_HISTORY_FILE, 'w') as query_history_file:
+                json.dump(queries, query_history_file)
+
     return queries
