955e2565e3ba574ec483535291bc12cb8a4a73e1
oravidov <or@singular.net>
2021-09-09 15:56:03 +0300

    cosmetics

--- lineage/dbt_utils.py
+++ lineage/dbt_utils.py
@@ -10,9 +10,16 @@ def connect_using_dbt_profiles(profiles_dir, profile_name):
     profile_type = dbt_profile.credentials.type
     if profile_type == 'snowflake':
         return snowflake.connector.connect(
+            account=dbt_profile.credentials.account,
             user=dbt_profile.credentials.user,
-            password=dbt_profile.credentials.password,
-            account=dbt_profile.credentials.account
+            database=dbt_profile.credentials.database,
+            schema=dbt_profile.credentials.schema,
+            warehouse=dbt_profile.credentials.warehouse,
+            role=dbt_profile.credentials.role,
+            autocommit=True,
+            client_session_keep_alive=dbt_profile.credentials.client_session_keep_alive,
+            application='elementary',
+            **dbt_profile.credentials.auth_args()
         )
     else:
         raise Exception("Unsupported profile type")
