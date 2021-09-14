37f12a5b67b18ef9f258f48127955acf9a72b042
oravi <oravi90@gmail.com>
2021-09-14 18:04:00 +0300

    Renamed lineage to edl (elementary data lineage)

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requirements = [x.strip() for x in package_requirements if ('git+' not i
 
 dependency_links = [x.strip().replace('git+', '') for x in package_requirements if 'git+' not in x]
 
-setup (
+setup(
     name='edl',
     description='Presenting data lineage based on your data warehouse query history',
     version='1.0.0',
@@ -27,7 +27,7 @@ setup (
     python_requires='>=3.6.2',
     entry_points='''
         [console_scripts]
-        lineage=lineage.main:main
+        edl=lineage.main:main
     ''',
     author="Elementary",
     keyword="data, lineage, data lineage, data warehouse, DWH",
