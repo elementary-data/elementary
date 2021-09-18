87fd1cfb056443a63fb4c35fb3415f4f2ec5c57f
oravidov <or@singular.net>
2021-09-18 13:49:45 +0300

    Added tests for query_history

---
+++ tests/test_query_history.py
@@ -0,0 +1,16 @@
+import pytest
+from unittest import mock
+from datetime import datetime
+
+from lineage.query_history import QueryHistory
+
+
+@pytest.mark.parametrize("end_date, expected_result", [
+    (datetime(2021, 9, 9), datetime(2021, 9, 9, 23, 59, 59)),
+    (datetime(2021, 9, 9, 11, 50, 3), datetime(2021, 9, 9, 11, 50, 3)),
+    (datetime(2021, 9, 9, 0, 0, 3), datetime(2021, 9, 9, 0, 0, 3)),
+    (datetime(2021, 9, 9, 0, 0, 0), datetime(2021, 9, 9, 23, 59, 59)),
+    (None, None),
+])
+def test_query_history_include_end_date(end_date, expected_result):
+    assert QueryHistory._include_end_date(end_date) == expected_result
