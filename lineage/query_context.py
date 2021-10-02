c9bb67fd86b7f67868285286646200b4273b1f97
oravidov <or@singular.net>
2021-10-02 18:22:37 +0300

    Added volume to query context, added to_html to query context

--- lineage/query_context.py
+++ lineage/query_context.py
@@ -18,6 +18,25 @@ class QueryContext(object):
                 'query_time': query_time_str,
                 'query_volume': self.query_volume}
 
+    def to_html(self):
+        if self.query_volume is not None and self.query_volume > 0:
+            volume_color = "RoyalBlue"
+            query_volume = self.query_volume
+        else:
+            volume_color = "tomato"
+            query_volume = 0
+
+        return f"""
+                    <html>
+                        <body>
+                            <a style=font-family:arial;color:RoyalBlue;">    
+                                Last update time: {self.query_time.strftime('%Y-%m-%d %H:%M:%S')}</br>
+                                Last update volume: <vol style="color:{volume_color};">{query_volume} rows</vol>
+                            </a>
+                        </body>
+                    </html>
+        """
+
     @staticmethod
     def from_dict(query_context_dict):
         if 'query_time' in query_context_dict and query_context_dict['query_time'] is not None:
