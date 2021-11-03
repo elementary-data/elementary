abf2e2781e50c5d1ba586687df4b94996f9d924a
oravidov <or@singular.net>
2021-11-03 13:08:30 +0200

    Printed instructions when result graph is empty

---
+++ lineage/empty_graph_helper.py
@@ -0,0 +1,24 @@
+
+
+class EmptyGraphHelper(object):
+    def __init__(self, platform_type):
+        self._platform_type = platform_type
+
+    def get_integration_docs_link(self):
+        if self._platform_type == 'snowflake':
+            return "https://docs.elementary-data.com/integrations/snowflake"
+        elif self._platform_type == 'bigquery':
+            return "https://docs.elementary-data.com/integrations/bigquery"
+        else:
+            return "https://docs.elementary-data.com/guides/usage/query-history-1"
+
+    def get_help_message(self):
+        return f"""
+        We are deeply sorry but the lineage graph is empty.\n
+        Please try the following steps to fix the problem - \n
+        \t1. Try running again with --ignore-schema=true (to avoid filtering queries running only on the schema 
+        configured in your profiles file) 
+        \t2. Check that your configured role in the profiles file has sufficient permissions
+        \t3. Follow this guide here - {self.get_integration_docs_link()}  
+        \t4. Join our slack channel here - https://bit.ly/slack-elementary, we promise to help and be nice! 
+        """
