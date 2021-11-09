d6f8dc8c946c2a81cf591d82eb36c0c2c6ac6e4c
oravi <oravi90@gmail.com>
2021-11-09 18:08:02 +0200

    Don't fail on bad query and fixed tests

--- lineage/snowflake_query.py
+++ lineage/snowflake_query.py
@@ -43,8 +43,8 @@ class SnowflakeQuery(Query):
 
             return True
 
-        except SQLLineageException as exc:
-            logger.debug(f'SQLLineageException was raised while parsing this query -\n{self._raw_query_text}\n'
+        except Exception as exc:
+            logger.debug(f'Exception was raised while parsing this query -\n{self._raw_query_text}\n'
                          f'Error was -\n{exc}.')
         return False
 
