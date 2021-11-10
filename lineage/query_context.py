d654971e2a43b919ac89738b88a4c62e3d7f4fdf
oravi <oravi90@gmail.com>
2021-11-10 17:46:05 +0200

    Added query_id

--- lineage/query_context.py
+++ lineage/query_context.py
@@ -10,7 +10,8 @@ class QueryContext(object):
                  query_time: Optional[datetime] = None, query_volume: Optional[int] = None,
                  query_type: Optional[str] = None, user_name: Optional[str] = None,
                  role_name: Optional[str] = None, referenced_tables: [dict] = None,
-                 destination_table: dict = None, duration: int = None) -> None:
+                 destination_table: dict = None, duration: int = None,
+                 query_id: str = None) -> None:
         self.queried_database = queried_database
         self.queried_schema = queried_schema
         self.query_time = query_time
@@ -21,6 +22,7 @@ class QueryContext(object):
         self.referenced_tables = referenced_tables if referenced_tables is not None else []
         self.destination_table = destination_table
         self.duration = duration
+        self.query_id = query_id
 
     def to_dict(self) -> dict:
         return {'queried_database': self.queried_database,
@@ -32,7 +34,8 @@ class QueryContext(object):
                 'role_name': self.role_name,
                 'referenced_tables': self.referenced_tables,
                 'destination_table': self.destination_table,
-                'duration': self.duration}
+                'duration': self.duration,
+                'query_id': self.query_id}
 
     @staticmethod
     def _query_time_to_str(query_time: Optional[datetime], fmt: str = None) -> Optional[str]:
@@ -54,6 +57,7 @@ class QueryContext(object):
         role_name = self._html_param_with_default(self.role_name)
         query_time = self._query_time_to_str(self.query_time, fmt='%Y-%m-%d %H:%M:%S')
         query_volume = self._html_param_with_default(self.query_volume, 0)
+        query_id = self._html_param_with_default(self.query_id)
         volume_color = "DarkSlateGrey"
         if query_volume == 0:
             volume_color = "tomato"
@@ -85,12 +89,13 @@ class QueryContext(object):
                                 <strong>
                                     Last update</br>
                                 </strong>
-                                <div style="min-width:62px;display:inline-block">Type:</div> {query_type}</br>
-                                <div style="min-width:62px;display:inline-block">User:</div> {user_name}</br>
-                                <div style="min-width:62px;display:inline-block">Role:</div> {role_name}</br>
-                                <div style="min-width:62px;display:inline-block">Time:</div> {query_time}</br>
-                                <div style="min-width:62px;display:inline-block">Duration:</div> {query_duration}</br>
-                                <div style="min-width:62px;display:inline-block;">Volume:</div> <a style="color:{volume_color}">{query_volume} rows</a>
+                                <div style="min-width:66px;display:inline-block">Type:</div> {query_type}</br>
+                                <div style="min-width:66px;display:inline-block">User:</div> {user_name}</br>
+                                <div style="min-width:66px;display:inline-block">Role:</div> {role_name}</br>
+                                <div style="min-width:66px;display:inline-block">Time:</div> {query_time}</br>
+                                <div style="min-width:66px;display:inline-block">Duration:</div> {query_duration}</br>
+                                <div style="min-width:66px;display:inline-block;">Volume:</div> <a style="color:{volume_color}">{query_volume} rows</a></br>
+                                <div style="min-width:66px;display:inline-block">Query ID:</div> {query_id}</br>
                             </div>
                         </body>
                     </html>
