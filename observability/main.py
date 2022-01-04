097580824a42055c629cdbb58bae0baecd148a21
oravi <oravi90@gmail.com>
2022-01-04 17:52:30 +0200

    WIP

--- observability/main.py
+++ observability/main.py
@@ -14,6 +14,8 @@ import webbrowser
 import os
 import subprocess
 from ruamel.yaml import YAML
+import glob
+from os.path import expanduser
 
 snowflake.connector.paramstyle = 'numeric'
 
@@ -23,6 +25,28 @@ print(f.renderText('Elementary'))
 ELEMENTARY_DBT_PACKAGE_NAME = 'elementary_observability'
 ELEMENTARY_DBT_PACKAGE = 'git@github.com:elementary-data/elementary-dbt.git'
 ELEMENTARY_DBT_PACKAGE_VERSION = 'master'
+
+yaml = YAML()
+yaml.indent(mapping=2, sequence=4, offset=2)
+yaml.default_flow_style
+yaml.preserve_quotes = True
+# import yaml
+# import yamlloader
+
+
+# TODO: move this to a view in our dbt pakcage
+SELECT_UNSTRUCTURED_COLUMNS_FROM_INFORMATION_SCHEMA = """
+                        SELECT column_name 
+                            FROM information_schema.columns 
+                            WHERE data_type in ('VARIANT') and collate(table_name, 'en-ci') = :2 and 
+                            collate(table_schema, 'en-ci') = :1;
+                        """
+# TODO: move this to a view in our dbt pakcage
+SELECT_UNSTRUCTURED_DATA = """
+                            SELECT IDENTIFIER(:1)
+                            FROM IDENTIFIER(:2)
+                            WHERE IDENTIFIER(:3) >= :4;
+                        """
 
 
 class RequiredIf(click.Option):
@@ -77,10 +101,6 @@ def init(ctx):
     elementary_dbt_package = {'git': ELEMENTARY_DBT_PACKAGE,
                               'revision': ELEMENTARY_DBT_PACKAGE_VERSION}
 
-    yaml = YAML()
-    yaml.indent(mapping=4, offset=4)
-    yaml.preserve_quotes = True
-
     print("Adding elementary data reliability package to your dbt packages.")
     dbt_packages = {'packages': []}
     if os.path.exists('packages.yml'):
@@ -128,94 +148,89 @@ def init(ctx):
 @elementary_data_reliability.command()
 @click.pass_context
 @click.option(
-    '--profiles-dir', '-d',
-    type=click.Path(exists=True),
-    help="You can connect to your data warehouse using your profiles dir, just specify your profiles dir where a "
-         "profiles.yml is located (could be a dbt profiles dir)."
-)
-@click.option(
-    '--profile-name', '-p',
-    type=str,
-    help="The profile name of the chosen profile in your profiles file."
-)
-@click.option(
-    '--table-name', '-t',
-    type=str,
-    default=None
-)
-@click.option(
-    '--json-column-name', '-j',
-    type=str,
-    default=None
-)
-@click.option(
-    '--time-column', '-c',
-    type=str,
-    default=None
-)
-@click.option(
     '--start-time', '-s',
     type=click.DateTime(formats=["%Y-%m-%d", "%Y-%m-%d %H:%M:%S"]),
-    default=str(date.today() - timedelta(days=6))
+    default=str(date.today() - timedelta(days=2))
 )
-@click.option(
-    '--file-name', '-f',
-    type=str,
-    default=None
-)
-def profile_sources(ctx, profiles_dir, profile_name, table_name, json_column_name, time_column, start_time, file_name):
+def profile_sources(ctx, start_time):
     click.echo(f"Debug is {'on' if ctx.obj['DEBUG'] else 'off'}")
 
-    builder = SchemaBuilder()
-    if file_name is not None:
-        with open(file_name, 'r') as jsons_file:
-            rows = jsons_file.readlines()
-            for row in tqdm(rows, desc=f"Extracting and parsing jsons from {file_name}", colour='green'):
-                builder.add_object(json.loads(row))
-        write_schema_json_to_output_files(builder.to_schema())
+    if not os.path.exists('dbt_project.yml'):
+        raise ConfigError('Please run this command from your main dbt project')
 
-    elif profiles_dir is not None and profile_name is not None:
-        credentials, profile_data = extract_credentials_and_data_from_profiles(profiles_dir, profile_name)
-        credentials_type = credentials.type
-        if credentials_type == 'snowflake':
-            snowflake_con = snowflake.connector.connect(
-                account=credentials.account,
-                user=credentials.user,
-                database=credentials.database,
-                schema=credentials.schema,
-                warehouse=credentials.warehouse,
-                role=credentials.role,
-                autocommit=True,
-                client_session_keep_alive=credentials.client_session_keep_alive,
-                application='elementary',
-                **credentials.auth_args()
-            )
+    with open('dbt_project.yml', 'r') as dbt_project_file:
+        #dbt_project = yaml.load(dbt_project_file, Loader=yamlloader.ordereddict.CSafeLoader)
+        dbt_project = yaml.load(dbt_project_file)
 
-            with snowflake_con.cursor() as cursor:
-                cursor.execute("""
-                                    SELECT IDENTIFIER(:1) 
-                                    FROM IDENTIFIER(:2) 
-                                    WHERE IDENTIFIER(:3) >= :4;
-                                """
-                               , (json_column_name,
-                                  table_name,
-                                  time_column,
-                                  start_time))
+    #TODO: make this a dbt utility
+    profile_name = dbt_project['profile']
+    profiles_dir = os.path.join(expanduser("~"), '.dbt')
+    credentials, profile_data = extract_credentials_and_data_from_profiles(profiles_dir, profile_name)
+    credentials_type = credentials.type
+    if credentials_type == 'snowflake':
+        snowflake_con = snowflake.connector.connect(
+            account=credentials.account,
+            user=credentials.user,
+            database=credentials.database,
+            schema=credentials.schema,
+            warehouse=credentials.warehouse,
+            role=credentials.role,
+            autocommit=True,
+            client_session_keep_alive=credentials.client_session_keep_alive,
+            application='elementary',
+            **credentials.auth_args()
+        )
+    else:
+        #TODO: use specific exception
+        raise ConfigError(f'Unsupported platform {credentials_type}')
 
-                rows = cursor.fetchall()
-                for row in tqdm(rows, desc=f"Extracting and parsing jsons from {table_name}", colour='green'):
-                    builder.add_object(json.loads(row[0]))
+    schema_file_paths = glob.glob(os.path.join('models', '*.yml'))
+    for schema_file_path in schema_file_paths:
+        with open(str(schema_file_path), 'r') as schema_file:
+            #model_schema = yaml.load(schema_file, Loader=yamlloader.ordereddict.CSafeLoader)
+            model_schema = yaml.load(schema_file)
+            if 'sources' in model_schema:
+                sources = model_schema['sources']
 
-        write_schema_json_to_output_files(builder.to_schema())
+                for source in sources:
+                    builder = SchemaBuilder()
+                    #TODO: make sure the name of the source is the schema?
+                    source_name = source.get('name')
+                    source_db = source.get('database', credentials.database)
+                    source_loaded_at_field = source.get('loaded_at_field')
+                    source_tables = source.get('tables')
 
-    if os.path.exists("my_schema.yml"):
-        with open("my_schema.yml", 'r') as schema_yml_file:
-            open('my_schema.json', 'w').write(json.dumps(yaml.load(schema_yml_file)))
+                    if source_name is None or source_loaded_at_field is None or source_tables is None:
+                        continue
 
-    config = GenerationConfiguration(copy_css=True, expand_buttons=True, with_footer=False, template_name='js',
-                                     show_toc=False)
-    generate_from_filename("my_schema.json", "schema_doc.html", config=config)
-    webbrowser.open('schema_doc.html')
+                    for source_table in source_tables:
+                        source_table_name = source_table.get('name')
+
+                        with snowflake_con.cursor() as cursor:
+                            cursor.execute('use database IDENTIFIER(:1)', (source_db,))
+                            cursor.execute(SELECT_UNSTRUCTURED_COLUMNS_FROM_INFORMATION_SCHEMA, (source_name,
+                                                                                                 source_table_name))
+
+                            rows = cursor.fetchall()
+                            for row in rows:
+                                unstructured_column = row[0]
+                                cursor.execute(SELECT_UNSTRUCTURED_DATA, (unstructured_column,
+                                                                          '.'.join([source_db,
+                                                                                    source_name,
+                                                                                    source_table_name]),
+                                                                          source_loaded_at_field,
+                                                                          start_time))
+                                rows = cursor.fetchall()
+                                for row in tqdm(rows, desc=f"Extracting and parsing unstructured data "
+                                                           f"from {source_name}.{source_table_name}", colour='green'):
+                                    builder.add_object(json.loads(row[0]))
+
+                    # TODO: Maybe should be under the column itself
+                    source['meta'] = {'json_schema': builder.to_schema()}
+
+        with open(str(schema_file_path), 'w') as schema_file:
+            #yaml.dump(model_schema, schema_file, width=100, Dumper=yamlloader.ordereddict.CDumper)
+            yaml.dump(model_schema, schema_file)
 
 
 if __name__ == "__main__":
