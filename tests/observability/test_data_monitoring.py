580c631215a7b0ea4218594f70d5d66f15036b6c
oravi <oravi90@gmail.com>
2022-01-22 19:24:49 +0200

    More tests work

--- tests/observability/test_data_monitoring.py
+++ tests/observability/test_data_monitoring.py
@@ -6,7 +6,7 @@ from unittest import mock
 from snowflake.connector.connection import SnowflakeConnection, SnowflakeCursor
 from observability.dbt_runner import DbtRunner
 from observability.config import Config
-from observability.data_monitoring import SnowflakeDataMonitoring
+from observability.data_monitoring import SnowflakeDataMonitoring, DataMonitoring
 
 SOURCES = {'sources': [{'name': 'unit_tests',
                         'database': 'elementary_tests',
@@ -44,6 +44,37 @@ def dbt_runner_mock():
     return mock.create_autospec(DbtRunner)
 
 
+@pytest.fixture
+def snowflake_data_monitoring_empty_config_in_db(config_mock, snowflake_con_mock, dbt_runner_mock):
+    # This mock cursor returns empty list to simulate empty configuration
+    snowflake_cursor_context_manager_return_value = snowflake_con_mock.cursor.return_value.__enter__.return_value
+    snowflake_cursor_context_manager_return_value.fetchall.return_value = []
+
+    snowflake_data_mon = SnowflakeDataMonitoring(config_mock, snowflake_con_mock)
+    snowflake_data_mon.dbt_runner = dbt_runner_mock
+    snowflake_data_mon._dbt_package_exists = lambda: True
+    return snowflake_data_mon
+
+
+@pytest.fixture
+def snowflake_data_monitoring(config_mock, snowflake_con_mock, dbt_runner_mock):
+    # This cursor mock will use the side effect to return non empty configuration
+    snowflake_cursor_context_manager_return_value = snowflake_con_mock.cursor.return_value.__enter__.return_value
+
+    def execute_query_side_effect(*args, **kwargs):
+        if 'count(*)' in args[0].lower():
+            snowflake_cursor_context_manager_return_value.fetchall.return_value = [[1]]
+        else:
+            snowflake_cursor_context_manager_return_value.fetchall.return_value = []
+
+    snowflake_cursor_context_manager_return_value.execute.side_effect = execute_query_side_effect
+
+    snowflake_data_mon = SnowflakeDataMonitoring(config_mock, snowflake_con_mock)
+    snowflake_data_mon.dbt_runner = dbt_runner_mock
+    snowflake_data_mon._dbt_package_exists = lambda: True
+    return snowflake_data_mon
+
+
 def assert_configuration_exists(data_monitoring):
     assert os.path.exists(data_monitoring.DBT_PROJECT_SEEDS_PATH)
     schema_config_csv_path = os.path.join(data_monitoring.DBT_PROJECT_SEEDS_PATH,
@@ -65,37 +96,79 @@ def delete_configuration(data_monitoring):
         shutil.rmtree(data_monitoring.DBT_PROJECT_SEEDS_PATH)
 
 
-def test_data_monitoring_run_monitoring_config_does_not_exist(config_mock, snowflake_con_mock, dbt_runner_mock):
-    # Scenario 1 - configuration doesn't exist yet
-    snowflake_cursor_context_manager_return_value = snowflake_con_mock.cursor.return_value.__enter__.return_value
-    snowflake_cursor_context_manager_return_value.fetchall.return_value = []
+def delete_dbt_package(data_monitoring):
+    if os.path.exists(data_monitoring.DBT_PROJECT_MODULES_PATH):
+        shutil.rmtree(data_monitoring.DBT_PROJECT_MODULES_PATH)
 
-    reference = SnowflakeDataMonitoring(config_mock, snowflake_con_mock)
-    reference.dbt_runner = dbt_runner_mock
+    if os.path.exists(data_monitoring.DBT_PROJECT_PACKAGES_PATH):
+        shutil.rmtree(data_monitoring.DBT_PROJECT_PACKAGES_PATH)
 
-    delete_configuration(reference)
-    reference.run()
-    assert_configuration_exists(reference)
+
+@pytest.mark.parametrize("full_refresh, update_dbt_package, reload_config", [
+    (True, True, False),
+    (True, False, False),
+    (True, True, True),
+    (True, False, True),
+    (False, True, False),
+    (False, False, False),
+    (False, True, True),
+    (False, False, True),
+])
+def test_data_monitoring_run_config_does_not_exist(full_refresh, update_dbt_package, reload_config,
+                                                   snowflake_data_monitoring_empty_config_in_db):
+    snowflake_data_monitoring = snowflake_data_monitoring_empty_config_in_db
+    delete_configuration(snowflake_data_monitoring)
+    delete_dbt_package(snowflake_data_monitoring)
+    dbt_runner_mock = snowflake_data_monitoring.dbt_runner
+
+    # The function we test
+    snowflake_data_monitoring.run(dbt_full_refresh=full_refresh, force_update_dbt_packages=update_dbt_package,
+                                  reload_monitoring_configuration=reload_config)
+
+    if update_dbt_package:
+        dbt_runner_mock.deps.assert_called()
+    else:
+        dbt_runner_mock.deps.assert_not_called()
+
+    # Validate configuration exists in the dbt_project seed path
+    assert_configuration_exists(snowflake_data_monitoring)
     dbt_runner_mock.seed.assert_called()
 
-
-def test_data_monitoring_run_with_reload_config_flag_true(config_mock, snowflake_con_mock, dbt_runner_mock):
-    # Scenario 2 - configuration exists but reload flag given
-    snowflake_cursor_context_manager_return_value = snowflake_con_mock.cursor.return_value.__enter__.return_value
-
-    def execute_query_side_effect(*args, **kwargs):
-        if 'count(*)' in args[0].lower():
-            snowflake_cursor_context_manager_return_value.fetchall.return_value = [[1]]
-        else:
-            snowflake_cursor_context_manager_return_value.fetchall.return_value = []
-
-    snowflake_cursor_context_manager_return_value.execute.side_effect = execute_query_side_effect
-
-    reference = SnowflakeDataMonitoring(config_mock, snowflake_con_mock)
-    reference.dbt_runner = dbt_runner_mock
-    delete_configuration(reference)
-    reference.run(reload_monitoring_configuration=True)
-    assert_configuration_exists(reference)
-    dbt_runner_mock.seed.assert_called()
+    # Validate that snapshot and run were called as well
+    dbt_runner_mock.snapshot.assert_called()
+    dbt_runner_mock.run.assert_called()
 
 
+@pytest.mark.parametrize("full_refresh, update_dbt_package, reload_config", [
+    (True, True, False),
+    (True, False, False),
+    (True, True, True),
+    (True, False, True),
+    (False, True, False),
+    (False, False, False),
+    (False, True, True),
+    (False, False, True),
+])
+def test_data_monitoring_run(full_refresh, update_dbt_package, reload_config, snowflake_data_monitoring):
+    delete_dbt_package(snowflake_data_monitoring)
+    delete_configuration(snowflake_data_monitoring)
+    dbt_runner_mock = snowflake_data_monitoring.dbt_runner
+
+    # The function we test
+    snowflake_data_monitoring.run(dbt_full_refresh=full_refresh, force_update_dbt_packages=update_dbt_package,
+                                  reload_monitoring_configuration=reload_config)
+
+    if update_dbt_package:
+        dbt_runner_mock.deps.assert_called()
+    else:
+        dbt_runner_mock.deps.assert_not_called()
+
+    if reload_config:
+        assert_configuration_exists(snowflake_data_monitoring)
+        dbt_runner_mock.seed.assert_called()
+    else:
+        dbt_runner_mock.seed.assert_not_called()
+
+    # Validate that dbt snapshot and dbt run were called as well
+    dbt_runner_mock.snapshot.assert_called()
+    dbt_runner_mock.run.assert_called_with(model=snowflake_data_monitoring.DBT_PACKAGE_NAME, full_refresh=full_refresh)
