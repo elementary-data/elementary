d053f20a4d3350cc01ad244003c6fb9f9cf46316
oravi <oravi90@gmail.com>
2022-01-23 14:54:27 +0200

    Moved all configuration logic to the Config object

--- observability/config.py
+++ observability/config.py
@@ -1,9 +1,9 @@
+import os
+import csv
 from typing import Union
 import glob
-
 from utils.dbt import get_model_paths_from_dbt_project, get_target_database_name
 from utils.ordered_yaml import OrderedYaml
-import os
 
 
 class Config(object):
@@ -31,7 +31,7 @@ class Config(object):
     def _find_schema_yml_files_in_dbt_project(dbt_project_models_path: str) -> list:
         return glob.glob(os.path.join(dbt_project_models_path, '*.yml'), recursive=True)
 
-    def get_dbt_project_sources(self) -> list:
+    def _get_sources_from_all_dbt_projects(self) -> list:
         monitoring_config = self._get_monitoring_configuration()
         dbt_projects = monitoring_config.get('dbt_projects', [])
         sources = []
@@ -49,3 +49,76 @@ class Config(object):
                                                'dbt_project_target_database': dbt_project_target_database}
                         sources.append(dbt_project_sources)
         return sources
+
+    @staticmethod
+    def _alert_on_schema_changes(source_dict: dict) -> Union[bool, None]:
+        metadata = source_dict.get('meta', {})
+        observability = metadata.get('observability', {})
+        alert_on_schema_changes = observability.get('alert_on_schema_changes')
+
+        # Normalize alert_on_schema_changes to handle both booleans and strings
+        alert_on_schema_changes_str = str(alert_on_schema_changes).lower()
+        if alert_on_schema_changes_str == 'false':
+            return False
+        elif alert_on_schema_changes_str == 'true':
+            return True
+        else:
+            return None
+
+    def monitoring_configuration_in_dbt_sources_to_csv(self, target_csv_path: str) -> None:
+        with open(target_csv_path, 'w') as target_csv:
+            target_csv_writer = csv.DictWriter(target_csv, fieldnames=['database_name',
+                                                                       'schema_name',
+                                                                       'table_name',
+                                                                       'column_name',
+                                                                       'alert_on_schema_changes'])
+            target_csv_writer.writeheader()
+
+            all_configured_sources = self._get_sources_from_all_dbt_projects()
+            for sources_dict in all_configured_sources:
+                sources = sources_dict.get('sources', [])
+                target_database = sources_dict.get('dbt_project_target_database')
+                for source in sources:
+                    source_db = source.get('database', target_database)
+                    if source_db is None:
+                        continue
+
+                    schema_name = source.get('schema', source.get('name'))
+                    if schema_name is None:
+                        continue
+
+                    alert_on_schema_changes = self._alert_on_schema_changes(source)
+                    if alert_on_schema_changes is not None:
+                        target_csv_writer.writerow({'database_name': source_db,
+                                                    'schema_name': schema_name,
+                                                    'table_name': None,
+                                                    'column_name': None,
+                                                    'alert_on_schema_changes': alert_on_schema_changes})
+
+                    source_tables = source.get('tables', [])
+                    for source_table in source_tables:
+                        table_name = source_table.get('identifier', source_table.get('name'))
+                        if table_name is None:
+                            continue
+
+                        alert_on_schema_changes = self._alert_on_schema_changes(source_table)
+                        if alert_on_schema_changes is not None:
+                            target_csv_writer.writerow({'database_name': source_db,
+                                                        'schema_name': schema_name,
+                                                        'table_name': table_name,
+                                                        'column_name': None,
+                                                        'alert_on_schema_changes': alert_on_schema_changes})
+
+                        source_columns = source_table.get('columns', [])
+                        for source_column in source_columns:
+                            column_name = source_column.get('name')
+                            if column_name is None:
+                                continue
+
+                            alert_on_schema_changes = self._alert_on_schema_changes(source_column)
+                            if alert_on_schema_changes is not None:
+                                target_csv_writer.writerow({'database_name': source_db,
+                                                            'schema_name': schema_name,
+                                                            'table_name': table_name,
+                                                            'column_name': column_name,
+                                                            'alert_on_schema_changes': alert_on_schema_changes})
--- tests/observability/test_config.py
+++ tests/observability/test_config.py
@@ -1,5 +1,6 @@
 import pytest
 import os
+import csv
 from utils.ordered_yaml import OrderedYaml
 from observability.config import Config
 
@@ -8,9 +9,22 @@ FILE_DIR = os.path.dirname(__file__)
 CONFIG = {'monitoring_configuration': {'slack_notification_webhook': 'test_slack_webhook',
                                        'dbt_projects': [FILE_DIR]}}
 
-SOURCES = {'sources': [{'name': 'unit_tests',
-                        'database': 'elementary_tests',
-                        'meta': {'observability': {'alert_on_schema_changes': 'true'}}}]}
+SOURCES = {'sources':
+                [{'name': 'unit_tests',
+                  'database': 'elementary_tests',
+                  'meta': {'observability': {'alert_on_schema_changes': 'true'}},
+                  'tables':
+                      [{'name': 'groups',
+                        'meta': {'observability': {'alert_on_schema_changes': 'true'}},
+                        'columns':
+                            [{'name': 'group_a',
+                              'meta': {'observability': {'alert_on_schema_changes': 'true'}}}]}]},
+                 {'schema': 'unit_tests_2',
+                  'database': 'elementary_tests_2',
+                  'meta': {'observability': {'alert_on_schema_changes': 'true'}},
+                  'tables':
+                      [{'identifier': 'groups_2',
+                        'meta': {'observability': {'alert_on_schema_changes': 'true'}}}]}]}
 
 DBT_PROJECT = {'name': 'elementary',
                'version': '1.0.0',
@@ -51,6 +65,15 @@ def create_config_files():
     yml.dump(PROFILES, os.path.join(FILE_DIR, 'profiles.yml'))
 
 
+def read_csv(csv_path):
+    csv_lines = []
+    with open(csv_path, 'r') as csvfile:
+        csv_reader = csv.DictReader(csvfile)
+        for row in csv_reader:
+            csv_lines.append(row)
+    return csv_lines
+
+
 @pytest.fixture
 def config():
     create_config_files()
@@ -61,9 +84,73 @@ def test_config_get_slack_notification_webhook(config):
     assert config.get_slack_notification_webhook() == CONFIG['monitoring_configuration']['slack_notification_webhook']
 
 
-def test_config_get_sources(config):
-    extracted_sources = config.get_dbt_project_sources()
+def test_config__get_sources_from_all_dbt_projects(config):
+    extracted_sources = config._get_sources_from_all_dbt_projects()
     assert len(extracted_sources[0]['sources']) == len(SOURCES['sources'])
     source_dict = extracted_sources[0]
     assert source_dict['sources'][0]['name'] == SOURCES['sources'][0]['name']
 
+
+@pytest.mark.parametrize("alert_on_schema_changes, expected_result", [
+    (True, True),
+    ('True', True),
+    ('true', True),
+    ('TrUe', True),
+    ('TRUE', True),
+    (False, False),
+    ('false', False),
+    ('False', False),
+    ('FALSE', False),
+    ('FaLSE', False),
+    (None, None),
+    ('bla', None),
+    ('None', None)
+])
+def test_config__alert_on_schema_changes(alert_on_schema_changes, expected_result, config):
+    source_dict = {'meta': {'observability': {'alert_on_schema_changes': alert_on_schema_changes}}}
+    assert config._alert_on_schema_changes(source_dict) is expected_result
+    source_dict = {'meta': {'observability': {'alert_on': True}}}
+    assert config._alert_on_schema_changes(source_dict) is None
+    source_dict = {'meta': {'observ': {'alert_on_schema_changes': True}}}
+    assert config._alert_on_schema_changes(source_dict) is None
+    source_dict = {'metadata': {'observability': {'alert_on_schema_changes': True}}}
+    assert config._alert_on_schema_changes(source_dict) is None
+
+
+def test_config_monitoring_configuration_in_dbt_sources_to_csv(config):
+    target_csv_path = os.path.join(FILE_DIR, 'monitoring_configuration.csv')
+
+    # The test function
+    config.monitoring_configuration_in_dbt_sources_to_csv(target_csv_path)
+
+    # Validate content of the generated csv file
+    monitoring_config_csv_lines = read_csv(target_csv_path)
+    assert monitoring_config_csv_lines[0]['database_name'] == SOURCES['sources'][0]['database']
+    assert monitoring_config_csv_lines[0]['schema_name'] == SOURCES['sources'][0]['name']
+    assert monitoring_config_csv_lines[0]['alert_on_schema_changes'].lower() == \
+           SOURCES['sources'][0]['meta']['observability']['alert_on_schema_changes'].lower()
+
+    assert monitoring_config_csv_lines[1]['database_name'] == SOURCES['sources'][0]['database']
+    assert monitoring_config_csv_lines[1]['schema_name'] == SOURCES['sources'][0]['name']
+    assert monitoring_config_csv_lines[1]['table_name'] == SOURCES['sources'][0]['tables'][0]['name']
+    assert monitoring_config_csv_lines[1]['alert_on_schema_changes'].lower() == \
+           SOURCES['sources'][0]['tables'][0]['meta']['observability']['alert_on_schema_changes'].lower()
+
+    assert monitoring_config_csv_lines[2]['database_name'] == SOURCES['sources'][0]['database']
+    assert monitoring_config_csv_lines[2]['schema_name'] == SOURCES['sources'][0]['name']
+    assert monitoring_config_csv_lines[2]['table_name'] == SOURCES['sources'][0]['tables'][0]['name']
+    assert monitoring_config_csv_lines[2]['column_name'] == \
+           SOURCES['sources'][0]['tables'][0]['columns'][0]['name']
+    assert monitoring_config_csv_lines[2]['alert_on_schema_changes'].lower() == \
+           SOURCES['sources'][0]['tables'][0]['columns'][0]['meta']['observability']['alert_on_schema_changes'].lower()
+
+    assert monitoring_config_csv_lines[3]['database_name'] == SOURCES['sources'][1]['database']
+    assert monitoring_config_csv_lines[3]['schema_name'] == SOURCES['sources'][1]['schema']
+    assert monitoring_config_csv_lines[3]['alert_on_schema_changes'].lower() == \
+           SOURCES['sources'][1]['meta']['observability']['alert_on_schema_changes'].lower()
+
+    assert monitoring_config_csv_lines[4]['database_name'] == SOURCES['sources'][1]['database']
+    assert monitoring_config_csv_lines[4]['schema_name'] == SOURCES['sources'][1]['schema']
+    assert monitoring_config_csv_lines[4]['table_name'] == SOURCES['sources'][1]['tables'][0]['identifier']
+    assert monitoring_config_csv_lines[4]['alert_on_schema_changes'].lower() == \
+           SOURCES['sources'][1]['tables'][0]['meta']['observability']['alert_on_schema_changes'].lower()
