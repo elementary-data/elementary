b0d7a76d655d1e2947b30f9dacfa951c21ee2e89
nimrodne <nimrod@shaked.dev>
2022-05-15 16:26:25 +0300

    MuiButtonGroup

--- frontend/src/alerts/components/Alerts.tsx
+++ frontend/src/alerts/components/Alerts.tsx
@@ -1,3 +1,15 @@
-const Alerts = () => <div>Alerts</div>;
+import { ButtonGroup, Button, Typography, Stack } from '@mui/material';
+import { FlexColumn } from 'shared/components';
+
+const Alerts = () => (
+  <Stack sx={{ padding: 2 }}>
+    <Typography sx={{ marginBottom: 5 }}>Alerts</Typography>
+    <ButtonGroup>
+      <Button>One</Button>
+      <Button>Two</Button>
+      <Button>Three</Button>
+    </ButtonGroup>
+  </Stack>
+);
 
 export default Alerts;
--- frontend/src/shared/style/theme.ts
+++ frontend/src/shared/style/theme.ts
@@ -1,5 +1,42 @@
 import { createTheme } from '@mui/material';
 
-const theme = createTheme({});
+const colors = {
+  white: '#FFFFFF',
+  black: '#273138',
+  gray: '#E6E8EC',
+  gray1: '#D4DCE7',
+  gray2: '#6B747A',
+};
+
+const theme = createTheme({
+  palette: {
+    common: colors,
+  },
+
+  components: {
+    MuiTypography: {
+      defaultProps: {
+        color: colors.black,
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none',
+        },
+      },
+    },
+    MuiButtonGroup: {
+      styleOverrides: {
+        grouped: {
+          background: colors.white,
+          color: colors.gray2,
+          borderColor: colors.gray1,
+          ':hover': { background: colors.gray },
+        },
+      },
+    },
+  },
+});
 
 export default theme;
