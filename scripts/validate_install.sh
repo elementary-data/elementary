83d1ac5cf488110b75d687bfa4a686ead6eb89a0
oravi <oravi90@gmail.com>
2022-05-18 15:23:44 +0300

    Added extra modules to validate_install.sh

--- scripts/validate_install.sh
+++ scripts/validate_install.sh
@@ -6,4 +6,10 @@ python3 -m ensurepip --upgrade
 python3 -m pip install --upgrade pip
 python3 -m pip install elementary-data
 python3 -c "import monitor"
+python3 -m pip install elementary-data[bigquery]
+python3 -m pip install elementary-data[snowflake]
+python3 -m pip install elementary-data[redshift]
+python3 -m pip install elementary-data[postgres]
+python3 -m pip install elementary-data[lineage]
+python3 -c "import lineage"
 deactivate
