fccd988bb0441f476238d5f45c4702dc219bb18c
IDoneShaveIt <idanshavit31@gmail.com>
2022-06-08 18:07:17 +0300

    Add support for vars argument in dbt runner methods

--- monitor/dbt_runner.py
+++ monitor/dbt_runner.py
@@ -1,6 +1,6 @@
 import json
 import subprocess
-from typing import Union
+from typing import Optional
 
 from utils.log import get_logger
 
@@ -10,7 +10,7 @@ logger = get_logger(__name__)
 class DbtRunner(object):
     ELEMENTARY_LOG_PREFIX = 'Elementary: '
 
-    def __init__(self, project_dir: str, profiles_dir: str, target: Union[str, None] = None) -> None:
+    def __init__(self, project_dir: str, profiles_dir: str, target: Optional[str] = None) -> None:
         self.project_dir = project_dir
         self.profiles_dir = profiles_dir
         self.target = target
@@ -40,7 +40,7 @@ class DbtRunner(object):
         success, _ = self._run_command(['deps'])
         return success
 
-    def seed(self, select: Union[str, None] = None) -> bool:
+    def seed(self, select: Optional[str] = None) -> bool:
         command_args = ['seed']
         if select is not None:
             command_args.extend(['-s', select])
@@ -51,12 +51,21 @@ class DbtRunner(object):
         success, _ = self._run_command(['snapshot'])
         return success
 
-    def run_operation(self, macro_name: str, json_logs: bool = True, macro_args: dict = None,
-                      log_errors: bool = False) -> list:
+    def run_operation(
+        self,
+        macro_name: str,
+        json_logs: bool = True,
+        macro_args: Optional[dict] = None,
+        log_errors: bool = False,
+        dbt_vars: Optional[dict] = None
+    ) -> list:
         command_args = ['run-operation', macro_name]
         if macro_args is not None:
             json_args = json.dumps(macro_args)
             command_args.extend(['--args', json_args])
+        if dbt_vars is not None:
+            json_vars = json.dumps(dbt_vars)
+            command_args.extend(['--vars', json_vars])
         success, command_output = self._run_command(command_args, json_logs)
         if log_errors and not success:
             logger.error(f'Failed to run macro: "{macro_name}"')
@@ -75,7 +84,13 @@ class DbtRunner(object):
                         run_operation_results.append(log_message.replace(self.ELEMENTARY_LOG_PREFIX, ''))
         return run_operation_results
 
-    def run(self, models: Union[str, None] = None, select: Union[str, None] = None, full_refresh: bool = False) -> bool:
+    def run(
+        self,
+        models: Optional[str] = None,
+        select: Optional[str] = None,
+        full_refresh: bool = False,
+        dbt_vars: Optional[dict] = None,
+    ) -> bool:
         command_args = ['run']
         if full_refresh:
             command_args.append('--full-refresh')
@@ -83,12 +98,18 @@ class DbtRunner(object):
             command_args.extend(['-m', models])
         if select is not None:
             command_args.extend(['-s', select])
+        if dbt_vars is not None:
+            json_vars = json.dumps(dbt_vars)
+            command_args.extend(['--vars', json_vars])
         success, _ = self._run_command(command_args)
         return success
 
-    def test(self, select: Union[str, None] = None):
+    def test(self, select: Optional[str] = None, dbt_vars: Optional[dict] = None) -> bool:
         command_args = ['test']
         if select is not None:
             command_args.extend(['-s', select])
+        if dbt_vars is not None:
+            json_vars = json.dumps(dbt_vars)
+            command_args.extend(['--vars', json_vars])
         success, _ = self._run_command(command_args)
         return success
