0be041bea1095c1ce6c90799bed29e2a3f63b9f0
IDoneShaveIt <idanshavit31@gmail.com>
2022-06-17 00:16:25 +0300

    Created send-report cli command

--- monitor/cli.py
+++ monitor/cli.py
@@ -7,6 +7,7 @@ from tracking.anonymous_tracking import AnonymousTracking, track_cli_start, trac
 from utils.ordered_yaml import OrderedYaml
 from config.config import Config
 from monitor.data_monitoring import DataMonitoring
+from clients.slack.slack_client import SlackClient
 
 yaml = OrderedYaml()
 
@@ -151,6 +152,67 @@ def report(ctx, config_dir, profiles_dir, update_dbt_package):
         track_cli_end(anonymous_tracking, 'monitor-report', data_monitoring.properties(), ctx.command.name)
     except Exception as exc:
         track_cli_exception(anonymous_tracking, 'monitor-report', exc, ctx.command.name)
+        raise
+    return
+
+
+@monitor.command()
+@click.option(
+    '--config-dir', '-c',
+    type=str,
+    default=os.path.join(expanduser('~'), '.edr'),
+    help="Global settings for edr are configured in a config.yml file in this directory "
+         "(if your config dir is HOME_DIR/.edr, no need to provide this parameter as we use it as default)."
+)
+@click.option(
+    '--profiles-dir', '-p',
+    type=str,
+    default=os.path.join(expanduser('~'), '.dbt'),
+    help="Specify your profiles dir where a profiles.yml is located, this could be a dbt profiles dir "
+         "(if your profiles dir is HOME_DIR/.dbt, no need to provide this parameter as we use it as default).",
+)
+@click.option(
+    '--update-dbt-package', '-u',
+    type=bool,
+    default=False,
+    help="Force downloading the latest version of the edr internal dbt package (usually this is not needed, "
+         "see documentation to learn more)."
+)
+@click.option(
+    '--slack-token', '-t',
+    type=str,
+    default=None,
+    help="A slack token for sending alerts over slack (also could be configured once in config.yml)"
+)
+@click.option(
+    '--slack-channel-name', '-ch',
+    type=str,
+    default=None,
+    help="The slack channel which all alerts will be sent to (also could be configured once in config.yml)"
+)
+@click.pass_context
+def send_report(
+    ctx,
+    config_dir,
+    profiles_dir,
+    update_dbt_package,
+    slack_token,
+    slack_channel_name
+):
+    config = Config(config_dir, profiles_dir)
+    anonymous_tracking = AnonymousTracking(config)
+    track_cli_start(anonymous_tracking, 'monitor-send-report', get_cli_properties(), ctx.command.name)
+    try:
+        data_monitoring = DataMonitoring(config, update_dbt_package)
+        data_monitoring.generate_report()
+        slack_client = SlackClient.initial(token=slack_token)
+        slack_client.upload_file(
+            channel_name=slack_channel_name,
+            file_path=os.path.join(config.target_dir, 'elementary.html'), 
+            message="Elemantary run report"
+        )
+    except Exception as exc:
+        track_cli_exception(anonymous_tracking, 'monitor-send-report', exc, ctx.command.name)
         raise
     return
 
