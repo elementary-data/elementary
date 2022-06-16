20bd656d51e5e76f26fe569349681f571b5ff542
oravi <oravi90@gmail.com>
2022-06-16 13:18:10 +0300

    consolidated the way we pull test samples

--- monitor/dbt_project/macros/get_new_alerts.sql
+++ monitor/dbt_project/macros/get_new_alerts.sql
@@ -1,8 +1,5 @@
 {% macro get_new_alerts(days_back, results_sample_limit = 5) %}
     -- depends_on: {{ ref('alerts') }}
-    {% set current_date = dbt_utils.date_trunc('day', dbt_utils.current_timestamp()) %}
-    --TODO: return last 30/7/1 without the sent filter
-    --TODO: group by to recieve latest?
     {% set select_new_alerts_query %}
         SELECT alert_id, model_unique_id, test_unique_id, detected_at, database_name, schema_name, table_name, column_name, alert_type, sub_type,
                alert_description, owners, tags, alert_results_query, other, test_name, test_params, severity, status
@@ -14,19 +11,13 @@
     {% for result in results %}
         {% set result_dict = result.dict() %}
         {% set alert_results_query = elementary.insensitive_get_dict_value(result_dict, 'alert_results_query') %}
-        {% set test_type = elementary.insensitive_get_dict_value(result_dict, 'test_type') %}
+        {% set alert_type = elementary.insensitive_get_dict_value(result_dict, 'alert_type') %}
         {% set status = elementary.insensitive_get_dict_value(result_dict, 'status') | lower %}
 
         {% set test_rows_sample = none %}
-        {% if alert_results_query and status != 'error' %}
-            {% set test_rows_sample_query = alert_results_query %}
-            {% if test_type == 'dbt_test' %}
-                {% set test_rows_sample_query = test_rows_sample_query ~ ' limit ' ~ results_sample_limit %}
-            {% endif %}
-
-            {% set test_rows_sample_agate = run_query(test_rows_sample_query) %}
-            {% set test_rows_sample = elementary.agate_to_json(test_rows_sample_agate) %}
-        {% endif %}
+        {%- if status != 'error'-%}
+            {% set test_rows_sample = elementary_internal.get_test_rows_sample(alert_results_query, alert_type, results_sample_limit) %}
+        {%- endif -%}
 
         {% set new_alert_dict = {'alert_id': elementary.insensitive_get_dict_value(result_dict, 'alert_id'),
                                  'model_unique_id': elementary.insensitive_get_dict_value(result_dict, 'model_unique_id'),
--- monitor/dbt_project/macros/get_test_results.sql
+++ monitor/dbt_project/macros/get_test_results.sql
@@ -18,48 +18,21 @@
 
         select * from latest_tests_in_the_last_30_days
     {%- endset -%}
-    {% set query_results_agate = run_query(select_test_results) %}
-    {% set query_results = elementary.agate_to_dicts(query_results_agate) %}
-    {% set test_results = [] %}
-    {%- for result_dict in query_results -%}
-        {% set test_results_query = elementary.insensitive_get_dict_value(result_dict, 'test_results_query') %}
-        {% set test_type = elementary.insensitive_get_dict_value(result_dict, 'test_type') %}
-        {% set status = elementary.insensitive_get_dict_value(result_dict, 'status') | lower %}
+    {% set elementary_test_results_agate = run_query(select_test_results) %}
+    {% set test_result_dicts = elementary.agate_to_dicts(elementary_test_results_agate) %}
+    {% set test_result_with_sample_dicts = [] %}
+    {%- for test_result_dict in test_result_dicts -%}
+        {% set test_results_query = elementary.insensitive_get_dict_value(test_result_dict, 'test_results_query') %}
+        {% set test_type = elementary.insensitive_get_dict_value(test_result_dict, 'test_type') %}
+        {% set status = elementary.insensitive_get_dict_value(test_result_dict, 'status') | lower %}
 
         {% set test_rows_sample = none %}
-        {% if test_results_query and status != 'error' %}
-            {% set test_query = test_results_query %}
-            {% if test_type == 'dbt_test' %}
-                {% set test_query = test_results_query ~ ' limit ' ~ results_sample_limit %}
-            {% endif %}
-
-            {% set test_rows_agate = run_query(test_query) %}
-            {% set test_rows_sample = elementary.agate_to_dicts(test_rows_agate) %}
-        {% endif %}
-
-        {% set new_alert_dict = {'alert_id': elementary.insensitive_get_dict_value(result_dict, 'id'),
-                                 'model_unique_id': elementary.insensitive_get_dict_value(result_dict, 'model_unique_id'),
-                                 'test_unique_id': elementary.insensitive_get_dict_value(result_dict, 'test_unique_id'),
-                                 'detected_at': elementary.insensitive_get_dict_value(result_dict, 'detected_at'),
-                                 'database_name': elementary.insensitive_get_dict_value(result_dict, 'database_name'),
-                                 'schema_name': elementary.insensitive_get_dict_value(result_dict, 'schema_name'),
-                                 'table_name': elementary.insensitive_get_dict_value(result_dict, 'table_name'),
-                                 'column_name': elementary.insensitive_get_dict_value(result_dict, 'column_name'),
-                                 'alert_type': test_type,
-                                 'sub_type': elementary.insensitive_get_dict_value(result_dict, 'test_sub_type'),
-                                 'alert_description': elementary.insensitive_get_dict_value(result_dict, 'test_results_description'),
-                                 'owners': elementary.insensitive_get_dict_value(result_dict, 'owners'),
-                                 'tags': elementary.insensitive_get_dict_value(result_dict, 'tags'),
-                                 'alert_results_query': test_results_query,
-                                 'alert_results': test_rows_sample,
-                                 'other': elementary.insensitive_get_dict_value(result_dict, 'other'),
-                                 'test_name': elementary.insensitive_get_dict_value(result_dict, 'test_name'),
-                                 'test_params': elementary.insensitive_get_dict_value(result_dict, 'test_params'),
-                                 'severity': elementary.insensitive_get_dict_value(result_dict, 'severity'),
-                                 'status': status,
-                                 'days_diff': elementary.insensitive_get_dict_value(result_dict, 'days_diff')} %}
-        {% do test_results.append(new_alert_dict) %}
+        {%- if status != 'error'-%}
+            {% set test_rows_sample = elementary_internal.get_test_rows_sample(test_results_query, test_type, results_sample_limit) %}
+        {%- endif -%}
+        {% do test_result_dict.update({'test_rows_sample': test_rows_sample}) %}
+        {% do test_result_with_sample_dicts.append(test_result_dict) %}
     {%- endfor -%}
-    {% do elementary.edr_log(tojson(test_results)) %}
+    {% do elementary.edr_log(tojson(test_result_with_sample_dicts)) %}
 {%- endmacro -%}
 
