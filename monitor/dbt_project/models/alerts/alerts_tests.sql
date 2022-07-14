cc805d16fe5dfcd97283fae935c8b272ef94dc23
Elon Gliksberg <elongliks@gmail.com>
2022-07-14 14:12:05 +0300

    Using a macro that returns the where clause of new alerts.

--- monitor/dbt_project/models/alerts/alerts_tests.sql
+++ monitor/dbt_project/models/alerts/alerts_tests.sql
@@ -6,12 +6,6 @@
   )
 }}
 
-{% set last_alert_time_with_backfill_query %}
-    {% set backfill_detected_at = elementary.timeadd('day', '-2', 'max(detected_at)') %}
-    select {{ backfill_detected_at }} from {{ this }}
-{% endset %}
-
-{% set last_alert_time_with_backfill = "'" ~ elementary.result_value(last_alert_time_with_backfill_query) ~ "'" %}
 
 with alerts_schema_changes as (
     select * from {{ ref('elementary', 'alerts_schema_changes') }}
@@ -35,15 +29,7 @@ all_alerts as (
 
 select *, false as alert_sent
 from all_alerts
+
 {%- if is_incremental() %}
-{%- set row_count = elementary.get_row_count(this) %}
-    {%- if row_count > 0 %}
-        where detected_at > {{ last_alert_time_with_backfill }}
-        and alert_id not in (
-            select alert_id
-            from {{ this }}
-            where detected_at > {{ last_alert_time_with_backfill }}
-            and alert_sent = true
-        )
-    {%- endif %}
+    {{ elementary.get_new_alerts_where_clause(this) }}
 {%- endif %}
