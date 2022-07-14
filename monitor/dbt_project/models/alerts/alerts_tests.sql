8aea3644cea1d8e3ea368d3f83da1b59cff5a168
Elon Gliksberg <elongliks@gmail.com>
2022-07-14 12:14:21 +0300

    Updated the backfill query.

--- monitor/dbt_project/models/alerts/alerts_tests.sql
+++ monitor/dbt_project/models/alerts/alerts_tests.sql
@@ -6,6 +6,13 @@
   )
 }}
 
+{% set last_alert_time_with_backfill_query %}
+    {% set backfill_detected_at = elementary.timeadd('day', '-2', 'max(detected_at)') %}
+    select {{ backfill_detected_at }} from {{ this }}
+{% endset %}
+
+{% set last_alert_time_with_backfill = "'" ~ elementary.result_value(last_alert_time_with_backfill_query) ~ "'" %}
+
 with alerts_schema_changes as (
     select * from {{ ref('elementary', 'alerts_schema_changes') }}
 ),
@@ -31,7 +38,12 @@ from all_alerts
 {%- if is_incremental() %}
 {%- set row_count = elementary.get_row_count(this) %}
     {%- if row_count > 0 %}
-        {% set backfill_detected_at = elementary.timeadd('day', '-2', 'max(detected_at)') %}
-        where detected_at > (select {{ backfill_detected_at }} from {{ this }})
+        where detected_at > {{ last_alert_time_with_backfill }}
+        and alert_id not in (
+            select alert_id
+            from {{ this }}
+            where detected_at > {{ last_alert_time_with_backfill }}
+            and alert_sent = true
+        )
     {%- endif %}
 {%- endif %}
