70f9666698c172dcad37c42b2d0c2f55bfa858fd
IDoneShaveIt <idanshavit31@gmail.com>
2022-07-17 14:46:33 +0300

    covert seed to source in lineage nodes ids

--- monitor/api/lineage/schema.py
+++ monitor/api/lineage/schema.py
@@ -2,6 +2,7 @@ from typing import List, Optional, Tuple
 from pydantic import BaseModel, validator
 from pydantic.typing import Literal
 import networkx as nx
+import re
 
 NodeUniqueIdType = str
 NodeType = Literal["model", "source", "exposure"] 
@@ -14,7 +15,21 @@ class NodeDependsOnNodesSchema(BaseModel):
 
     @validator("depends_on_nodes", pre=True, always=True)
     def set_depends_on_nodes(cls, depends_on_nodes):
-        return depends_on_nodes or []
+        formatted_depends_on = depends_on_nodes or []
+        return [cls._format_node_id(node_id) for node_id in formatted_depends_on]
+    
+    @validator("unique_id", pre=True, always=True)
+    def set_unique_id(cls, unique_id):
+        return cls._format_node_id(unique_id)
+    
+    @classmethod
+    def _format_node_id(cls, node_id: str):
+        # Currently seeds are saved as sources.
+        # Convert seed prefix into source for nodes unique ids.
+        seed_patern = re.compile(r"^seed\.")
+        if re.search(seed_patern, node_id):
+            return re.sub(seed_patern, "source.", node_id, 1)
+        return node_id
 
 
 class LineageNodeSchema(BaseModel):
