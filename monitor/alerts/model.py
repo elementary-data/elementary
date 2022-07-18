1ec85866448bdf2a13e0552731ab408e5b3b239d
IDoneShaveIt <idanshavit31@gmail.com>
2022-07-18 13:23:36 +0300

    WIP

--- monitor/alerts/model.py
+++ monitor/alerts/model.py
@@ -1,7 +1,7 @@
 import json
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Union
+from typing import List, Union
 
 from clients.slack.schema import SlackMessageSchema
 from monitor.alerts.alert import Alert
@@ -22,12 +22,14 @@ class ModelAlert(Alert):
     owners: str
     tags: str
     status: str
+    subscribers: List[str]
 
     TABLE_NAME = 'alerts_models'
 
     def __post_init__(self):
         self.owners = prettify_json_str_set(self.owners)
         self.tags = prettify_json_str_set(self.tags)
+        self.subscribers = prettify_json_str_set(self.subscribers)
 
     def to_slack(self, is_slack_workflow: bool = False) -> SlackMessageSchema:
         icon = ':small_red_triangle:'
@@ -41,7 +43,7 @@ class ModelAlert(Alert):
                                               [f'*Model*\n{self.alias}',
                                                f'*When*\n{self.detected_at}'],
                                               divider=True)
-        self._add_fields_section_to_slack_msg(slack_message, [f'*Owners*\n{self.owners}', f'*Tags*\n{self.tags}'])
+        self._add_fields_section_to_slack_msg(slack_message, [f'*Owners*\n{self.owners}', f'*Subscribers*\n{self.subscribers}' f'*Tags*\n{self.tags}'])
         self._add_text_section_to_slack_msg(slack_message, f'*Error Message*\n```{self.message}```')
         self._add_fields_section_to_slack_msg(slack_message,
                                               [f'*Full Refresh*\n{self.full_refresh}', f'*Path*\n{self.path}'],
