d8626ba1e7008b69c85c954d0e2ef42cd3f8e31e
Elon Gliksberg <elongliks@gmail.com>
2022-07-20 19:59:53 +0300

    Checking that relations exist before querying them.

--- monitor/dbt_project/models/alerts/alerts_models.sql
+++ monitor/dbt_project/models/alerts/alerts_models.sql
@@ -8,8 +8,11 @@
 }}
 
 
-select *, false as alert_sent from {{ ref('elementary', 'error_models') }}
-
-{%- if is_incremental() %}
-    where {{ get_new_alerts_where_clause(this) }}
-{%- endif %}
+{% set elementary_database, elementary_schema = elementary.get_package_database_and_schema() %}
+{% set error_models_relation = adapter.get_relation(elementary_database, elementary_schema, 'error_models') %}
+{% if error_models_relation %}
+    select *, false as alert_sent from {{ error_models_relation }}
+    {% if is_incremental() %}
+        where {{ get_new_alerts_where_clause(this) }}
+    {% endif %}
+{% endif %}
