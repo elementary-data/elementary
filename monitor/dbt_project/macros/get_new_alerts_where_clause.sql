53a032ffd79e1a6b2207b8f3915e9cc50f0e42e1
Elon Gliksberg <elongliks@gmail.com>
2022-07-21 15:34:33 +0300

    Returning 'false' instead of 'none', checking using 'last_alert_time_with_backfill' rather than row count.

--- monitor/dbt_project/macros/get_new_alerts_where_clause.sql
+++ monitor/dbt_project/macros/get_new_alerts_where_clause.sql
@@ -5,20 +5,19 @@
         select {{ backfill_detected_at }} from {{ alerts_model_relation }}
     {% endset %}
 
-    {% set last_alert_time_with_backfill = "'" ~ elementary.result_value(last_alert_time_with_backfill_query) ~ "'" %}
+    {% set last_alert_time_with_backfill = elementary.result_value(last_alert_time_with_backfill_query) %}
 
-    {%- set row_count = elementary.get_row_count(alerts_model_relation) %}
-    {%- if row_count > 0 %}
+    {%- if last_alert_time_with_backfill %}
         {% set where_clause %}
-            detected_at > {{ last_alert_time_with_backfill }}
+            detected_at > '{{ last_alert_time_with_backfill }}'
             and alert_id not in (
                 select alert_id
                 from {{ alerts_model_relation }}
-                where detected_at > {{ last_alert_time_with_backfill }}
+                where detected_at > '{{ last_alert_time_with_backfill }}'
                 and alert_sent = true
             )
         {% endset %}
         {{ return(where_clause) }}
     {%- endif %}
-    {{ return(none) }}
+    {{ return(false) }}
 {% endmacro %}
