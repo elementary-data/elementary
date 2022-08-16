7ae8e23d5cf6414b14f8918fd9472fafad040eda
Elon Gliksberg <elongliks@gmail.com>
2022-08-16 10:46:02 +0300

    Attempting to join if not in channel.

--- clients/slack/client.py
+++ clients/slack/client.py
@@ -1,6 +1,6 @@
 import json
 from abc import ABC, abstractmethod
-from typing import Optional
+from typing import Optional, List
 
 from slack_sdk import WebClient, WebhookClient
 from slack_sdk.errors import SlackApiError
@@ -54,12 +54,26 @@ class SlackWebClient(SlackClient):
         try:
             self.client.chat_postMessage(
                 channel=channel_name,
-                text=message.text or 'Elementary Data',
+                text=message.text,
                 blocks=json.dumps(message.blocks) if message.blocks else None,
                 attachments=json.dumps(message.attachments) if message.attachments else None
             )
             return True
-        except SlackApiError as e:
+        except SlackApiError as err:
+            err_type = err.response.data['error']
+            if err_type == 'not_in_channel':
+                logger.info('Elementary app is not in the channel. Attempting to join.')
+                channel_id = self._get_channel_id(channel_name)
+                if not channel_id:
+                    return False
+                joined_successfully = self._join_channel(channel_id=channel_id)
+                if not joined_successfully:
+                    return False
+                self.send_message(channel_name=channel_name, message=message)
+            elif err_type == 'channel_not_found':
+                logger.error(
+                    f'Channel was {channel_name} not found by the Elementary app. Please add the app to the channel.')
+                return False
             logger.error(f"Could not post message to channel - {channel_name}. Error: {e}")
             return False
 
@@ -75,9 +89,9 @@ class SlackWebClient(SlackClient):
             logger.error(f"Could not upload the file to the channel - {channel_name}. Error: {e}")
             return False
 
-    def send_report(self, channel_name: str, report_file_path: str):
+    def send_report(self, channel: str, report_file_path: str):
         send_succeed = self.send_file(
-            channel_name=channel_name,
+            channel_name=channel,
             file_path=report_file_path,
             message=SlackMessageSchema(text="Elementary monitoring report")
         )
@@ -86,6 +100,43 @@ class SlackWebClient(SlackClient):
         else:
             logger.error('Failed to send report to Slack.')
         return send_succeed
+
+    def _get_channel_id(self, channel_name: str) -> Optional[str]:
+        try:
+            channels = self._get_channels()
+            for channel in channels:
+                if channel['name'] == channel_name:
+                    return channel['id']
+            available_channel_names = [channel['name'] for channel in channels]
+            logger.error(f'Channel {channel_name} not found. Available channels: {available_channel_names}')
+            return None
+        except Exception:
+            logger.error(f'Elementary app failed to query Slack channels.')
+            return None
+
+    def _join_channel(self, channel_id: str) -> bool:
+        try:
+            self.client.conversations_join(channel=channel_id)
+            return True
+        except SlackApiError as e:
+            logger.error(f"Elementary app failed to join the given channel. Error: {e}")
+            return False
+
+    def _get_channels(self) -> List[dict]:
+        try:
+            channels = []
+            has_more = True
+            cursor = None
+            while has_more:
+                response = self.client.conversations_list(cursor=cursor, types='public_channel,private_channel',
+                                                          exclude_archived=True)
+                channels.extend(response["channels"])
+                cursor = response.get("response_metadata", {}).get("next_cursor")
+                has_more = True if cursor else False
+            return channels
+        except SlackApiError as e:
+            logger.error(f"Elementary app failed to query all Slack public channels. Error: {e}")
+            return []
 
 
 class SlackWebhookClient(SlackClient):
