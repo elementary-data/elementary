42a7c665c54c6c956008aa2712d8fa50277e7759
Elon Gliksberg <elongliks@gmail.com>
2022-08-22 11:28:51 +0300

    Renamed inner macros.

--- monitor/dbt_project/macros/get_adapter_type_and_unique_id.sql
+++ monitor/dbt_project/macros/get_adapter_type_and_unique_id.sql
@@ -1,15 +1,15 @@
 {% macro get_adapter_type_and_unique_id() %}
-    {{ elementary.edr_log(tojson([target.type, adapter.dispatch('get_adapter_type_and_unique_id')()])) }}
+    {{ elementary.edr_log(tojson([target.type, adapter.dispatch('get_adapter_unique_id')()])) }}
 {%- endmacro %}
 
-{% macro default__get_adapter_type_and_unique_id() %}
+{% macro default__get_adapter_unique_id() %}
     {{ return(target.host) }}
 {% endmacro %}
 
-{% macro snowflake__get_adapter_type_and_unique_id() %}
+{% macro snowflake__get_adapter_unique_id() %}
     {{ return(target.account) }}
 {% endmacro %}
 
-{% macro bigquery__get_adapter_type_and_unique_id() %}
+{% macro bigquery__get_adapter_unique_id() %}
     {{ return(target.project) }}
 {% endmacro %}
