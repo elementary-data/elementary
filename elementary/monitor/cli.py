b711bc5343e83a475a0090b812a3df6bce58343a
IDoneShaveIt <idanshavit31@gmail.com>
2022-09-07 14:00:53 +0300

    Splited file-name to slack-file-name and bucket-file-path arguments

--- elementary/monitor/cli.py
+++ elementary/monitor/cli.py
@@ -1,3 +1,4 @@
+from email.policy import default
 import os
 import sys
 
@@ -198,6 +199,12 @@ def report(ctx, days_back, config_dir, profiles_dir, update_dbt_package, profile
     help="The slack channel which all alerts will be sent to.",
 )
 @click.option(
+    '--slack-file-name',
+    type=str,
+    default=None,
+    help="The report's file name, this is how it will be sent to slack."
+)
+@click.option(
     '--aws-profile-name',
     type=str,
     default=None,
@@ -246,9 +253,10 @@ def report(ctx, days_back, config_dir, profiles_dir, update_dbt_package, profile
     help='Set the number of invocations shown for each test in the "Test Runs" report.'
 )
 @click.option(
-    '--file-name',
+    '--bucket-file-path',
     type=str,
-    help="The report's file name, this is how it will be stored in the bucket."
+    default=None,
+    help="The report's file name, this is where it will be stored in the bucket (may contain folders)."
 )
 @click.option(
     '--disable-passed-test-metrics',
@@ -265,9 +273,10 @@ def send_report(
         update_dbt_package,
         slack_token,
         slack_channel_name,
+        slack_file_name,
         profile_target,
         executions_limit,
-        file_name,
+        bucket_file_path,
         disable_passed_test_metrics,
         update_bucket_website,
         aws_profile_name,
@@ -294,14 +303,14 @@ def send_report(
         config.validate_send_report()
         # If file name contains a folders (is a path),
         # we want to make sure that we create the report localy without the folders of the full path.
-        local_file_path = os.path.basename(file_name) if file_name else None
+        local_file_path = os.path.basename(bucket_file_path) if bucket_file_path else slack_file_name
         data_monitoring = DataMonitoring(config=config, force_update_dbt_package=update_dbt_package)
         command_succeeded = False
         generated_report_successfully, elementary_html_path = data_monitoring.generate_report(
             tracking=anonymous_tracking, days_back=days_back, test_runs_amount=executions_limit,
             disable_passed_test_metrics=disable_passed_test_metrics, file_path=local_file_path, should_open_browser=False)
         if generated_report_successfully and elementary_html_path:
-            command_succeeded = data_monitoring.send_report(elementary_html_path, remote_file_path=file_name)
+            command_succeeded = data_monitoring.send_report(elementary_html_path, remote_file_path=bucket_file_path)
         anonymous_tracking.track_cli_end('monitor-send-report', data_monitoring.properties(), ctx.command.name)
         if not command_succeeded:
             sys.exit(1)
