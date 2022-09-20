894b5f423282e242c539657bb66f19cae05c3522
IDoneShaveIt <idanshavit31@gmail.com>
2022-09-20 19:56:50 +0300

    add model unique_id to the elementary unique_id creation

--- elementary/monitor/dbt_project/macros/cte/current_tests_run_results.sql
+++ elementary/monitor/dbt_project/macros/cte/current_tests_run_results.sql
@@ -66,8 +66,8 @@
         dbt_tests_with_elementary_unique_id as (
             select 
                 case 
-                    when (alias = name and alias is not null and test_column_name is not null and short_name is not null) then test_column_name || '.' || alias || '.' || short_name
-                    when (alias = name and alias is not null and short_name is not null) then alias || '.' || short_name
+                    when (alias = name and alias is not null and test_column_name is not null and short_name is not null) then parent_model_unique_id || '.' || test_column_name || '.' || alias || '.' || short_name
+                    when (alias = name and alias is not null and short_name is not null) then parent_model_unique_id || '.' || alias || '.' || short_name
                     else unique_id
                 end as elementary_unique_id,
                 unique_id,
@@ -144,8 +144,8 @@
         elementary_test_results_with_elementary_unique_id as (
             select
                 case 
-                    when (test_alias = test_name and test_alias is not null and column_name is not null and test_short_name is not null) then column_name || '.' || test_alias || '.' || test_short_name
-                    when (test_alias = test_name and test_alias is not null and test_short_name is not null) then test_alias || '.' || test_short_name
+                    when (test_alias = test_name and test_alias is not null and column_name is not null and test_short_name is not null) then model_unique_id || '.' || column_name || '.' || test_alias || '.' || test_short_name
+                    when (test_alias = test_name and test_alias is not null and test_short_name is not null) then model_unique_id || '.' || test_alias || '.' || test_short_name
                     else test_unique_id
                 end as elementary_unique_id,
                 id,
