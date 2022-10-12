10e2dd420fccf248bd3d89e2683ffafab3666d9d
Elon Gliksberg <elongliks@gmail.com>
2022-10-12 11:27:08 +0300

    Changed source freshness empty table definition.

--- elementary/monitor/dbt_project/macros/empty_alert_tables.sql
+++ elementary/monitor/dbt_project/macros/empty_alert_tables.sql
@@ -3,5 +3,5 @@
 {% endmacro %}
 
 {% macro empty_alerts_source_freshness() %}
-	{{ elementary.empty_table([('alert_id', 'string'), ('unique_id', 'string'), ('detected_at', 'string'), ('max_loaded_at', 'string'), ('status', 'string'), ('alert_sent', 'boolean')]) }}
+	{{ elementary.empty_table([('alert_id', 'string'), ('max_loaded_at', 'string'), ('snapshotted_at', 'string'), ('detected_at', 'string'), ('max_loaded_at_time_ago_in_s', 'string'), ('status', 'string'), ('error', 'string'), ('unique_id', 'string'), ('database_name', 'string'), ('schema_name', 'string'), ('source_name', 'string'), ('identifier', 'string'), ('freshness_error_after', 'string'), ('freshness_warn_after', 'string'), ('freshness_filter', 'string'), ('tags', 'string'), ('meta', 'string'), ('owner', 'string'), ('package_name', 'string'), ('path', 'string'), ('alert_sent', 'string')]) }}
 {% endmacro %}
