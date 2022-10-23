52e8aba0d8ed36804bd9e983c4f133a6da22097a
Elon Gliksberg <elongliks@gmail.com>
2022-10-23 17:07:17 +0300

    Renamed 'matrix' to 'map'.

--- elementary/utils/package.py
+++ elementary/utils/package.py
@@ -8,8 +8,8 @@ from packaging import version
 from elementary.exceptions.exceptions import IncompatibleDbtPackageError
 
 _PYPI_URL = "https://pypi.org/pypi/elementary-data/json"
-_COMPATIBILITY_LAYER_URL = (
-    "https://storage.googleapis.com/elementary_static/edr_dbt_compatibility_matrix.json"
+_COMPATIBILITY_MAP_URL = (
+    "https://storage.googleapis.com/elementary_static/edr_dbt_compatibility_map.json"
 )
 
 
@@ -29,26 +29,26 @@ def get_latest_package_version() -> Optional[str]:
         return None
 
 
-def _get_compatibility_matrix() -> Dict[version.Version, Dict[str, version.Version]]:
-    versioned_compatibility_matrix = {}
-    str_compatibility_matrix = requests.get(_COMPATIBILITY_LAYER_URL).json()
-    for edr_ver, dbt_ver_range in str_compatibility_matrix.items():
-        versioned_compatibility_matrix[version.parse(edr_ver)] = {
+def _get_compatibility_map() -> Dict[version.Version, Dict[str, version.Version]]:
+    versioned_compatibility_map = {}
+    str_compatibility_map = requests.get(_COMPATIBILITY_MAP_URL).json()
+    for edr_ver, dbt_ver_range in str_compatibility_map.items():
+        versioned_compatibility_map[version.parse(edr_ver)] = {
             "min": version.parse(dbt_ver_range["min"]),
             "max": version.parse(dbt_ver_range["max"]),
         }
-    return versioned_compatibility_matrix
+    return versioned_compatibility_map
 
 
 def check_dbt_pkg_compatible(current_dbt_pkg_ver: str):
     current_dbt_pkg_ver = version.parse(current_dbt_pkg_ver)
     current_py_pkg_ver = version.parse(get_package_version())
-    compatibility_matrix = _get_compatibility_matrix()
-    sorted_py_pkg_vers = sorted(compatibility_matrix)
+    compatibility_map = _get_compatibility_map()
+    sorted_py_pkg_vers = sorted(compatibility_map)
     relevant_py_pkg_index = bisect.bisect(sorted_py_pkg_vers, current_py_pkg_ver) - 1
     if relevant_py_pkg_index == -1:
         return
-    dbt_pkg_range = compatibility_matrix[sorted_py_pkg_vers[relevant_py_pkg_index]]
+    dbt_pkg_range = compatibility_map[sorted_py_pkg_vers[relevant_py_pkg_index]]
 
     if not (dbt_pkg_range["min"] <= current_dbt_pkg_ver <= dbt_pkg_range["max"]):
         raise IncompatibleDbtPackageError(current_py_pkg_ver, dbt_pkg_range)
