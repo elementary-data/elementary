0f0311fe582b63159fe566f6dac61a458f051609
Elon Gliksberg <elongliks@gmail.com>
2022-10-23 20:39:35 +0300

    Incompatible max is optional.

--- elementary/utils/package.py
+++ elementary/utils/package.py
@@ -49,6 +49,9 @@ def check_dbt_pkg_compatible(current_dbt_pkg_ver: str):
     if relevant_py_pkg_index == -1:
         return
     dbt_pkg_range = compatibility_map[sorted_py_pkg_vers[relevant_py_pkg_index]]
-
-    if not (dbt_pkg_range["min"] <= current_dbt_pkg_ver <= dbt_pkg_range["max"]):
+    incompatible_min = current_dbt_pkg_ver < dbt_pkg_range["min"]
+    incompatible_max = (
+        "max" in dbt_pkg_range and current_dbt_pkg_ver > dbt_pkg_range["max"]
+    )
+    if incompatible_min or incompatible_max:
         raise IncompatibleDbtPackageError(current_py_pkg_ver, dbt_pkg_range)
