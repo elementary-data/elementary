5d39b9f1a10a8fe6d37010400e96ed634f455092
Elon Gliksberg <elongliks@gmail.com>
2022-10-30 12:26:30 +0200

    Updated get_elementary_dbt_pkg_version().

--- elementary/monitor/dbt_project/macros/get_elementary_dbt_pkg_version.sql
+++ elementary/monitor/dbt_project/macros/get_elementary_dbt_pkg_version.sql
@@ -1,13 +1,13 @@
 {% macro get_elementary_dbt_pkg_version() %}
   {% set database, schema = elementary.target_database(), target.schema %}
-  {% set information_relation = adapter.get_relation(database, schema, 'information') %}
-  {% if not information_relation %}
+  {% set invocations_relation = adapter.get_relation(database, schema, 'dbt_invocations') %}
+  {% if not invocations_relation %}
     {% do elementary.edr_log('') %}
     {% do return(none) %}
   {% endif %}
 
   {% set get_pkg_version_query %}
-    select value from {{ information_relation }} where key = 'elementary_version'
+    select elementary_version from {{ invocations_relation }} order by generated_at desc limit 1
   {% endset %}
   {% set result = dbt.run_query(get_pkg_version_query)[0][0] %}
   {% do elementary.edr_log(result or '') %}
