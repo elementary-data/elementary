ac8e4f92bda34ada1a2e168502b96acd9f8441ea
Elon Gliksberg <elongliks@gmail.com>
2022-11-28 16:55:04 +0200

    Passing 'project_name' and 'target_name' to the report.

--- elementary/monitor/dbt_project/macros/get_latest_invocation.sql
+++ elementary/monitor/dbt_project/macros/get_latest_invocation.sql
@@ -1,4 +1,4 @@
-{% macro get_elementary_dbt_pkg_version() %}
+{% macro get_latest_invocation() %}
   {% set database, schema = elementary.target_database(), target.schema %}
   {% set invocations_relation = adapter.get_relation(database, schema, 'dbt_invocations') %}
   {% if not invocations_relation %}
@@ -7,8 +7,8 @@
   {% endif %}
 
   {% set get_pkg_version_query %}
-    select elementary_version from {{ invocations_relation }} order by generated_at desc limit 1
+    select * from {{ invocations_relation }} order by generated_at desc limit 1
   {% endset %}
-  {% set result = dbt.run_query(get_pkg_version_query)[0][0] %}
+  {% set result = elementary.agate_to_json(dbt.run_query(get_pkg_version_query)) %}
   {% do elementary.edr_log(result or '') %}
 {% endmacro %}
