be42966df3c5776247f7d4da4037685212d82d98
IDoneShaveIt <idanshavit31@gmail.com>
2022-12-20 18:35:51 +0200

    fixed invocation selected format

--- elementary/monitor/api/invocations/schema.py
+++ elementary/monitor/api/invocations/schema.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 from pydantic import BaseModel, validator
 
+from elementary.utils.json_utils import try_load_json
 from elementary.utils.time import convert_partial_iso_format_to_full_iso_format
 
 
@@ -15,3 +16,8 @@ class DbtInvocationSchema(BaseModel):
     @validator("detected_at", pre=True)
     def format_detected_at(cls, detected_at):
         return convert_partial_iso_format_to_full_iso_format(detected_at)
+
+    @validator("selected", pre=True)
+    def format_selected(cls, selected):
+        selected_list = try_load_json(selected) or []
+        return " ".join(selected_list)
