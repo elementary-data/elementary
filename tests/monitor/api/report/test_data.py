576f0e5c22d83acab4bfabb6140c085ea387654b
Elon Gliksberg <elongliks@gmail.com>
2022-12-29 15:40:31 +0200

    Testing totals of report.

---
+++ tests/monitor/api/report/test_data.py
@@ -0,0 +1,35 @@
+import json
+from pathlib import Path
+
+import pytest
+
+_REPORT_DATA_FILENAME = "elementary_output.json"
+_REPORT_DATA_FIXTURE = Path(__file__).parent / "fixtures" / _REPORT_DATA_FILENAME
+_REPORT_DATA_PATH = Path(_REPORT_DATA_FILENAME)
+
+
+def does_report_data_exist():
+    return _REPORT_DATA_PATH.exists()
+
+
+def report_data_exists_marker():
+    return pytest.mark.skipif(
+        not does_report_data_exist(), reason="Report data does not exist."
+    )
+
+
+@pytest.fixture
+def report_data():
+    return json.loads(_REPORT_DATA_PATH.read_text())
+
+
+@pytest.fixture
+def report_data_fixture():
+    return json.loads(_REPORT_DATA_FIXTURE.read_text())
+
+
+@report_data_exists_marker()
+def test_totals(report_data, report_data_fixture):
+    for key in report_data:
+        if key.endswith("_totals"):
+            assert report_data[key] == report_data_fixture[key]
