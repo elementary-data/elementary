419c5f8ec052dd7e73ba3f8e164ed921c195829e
Elon Gliksberg <elongliks@gmail.com>
2023-01-01 10:51:03 +0200

    Tests restructure file system.

--- tests/integration/report/test_report.py
+++ tests/integration/report/test_report.py
@@ -8,16 +8,6 @@ _REPORT_DATA_FIXTURE = Path(__file__).parent / "fixtures" / _REPORT_DATA_FILENAM
 _REPORT_DATA_PATH = Path(_REPORT_DATA_FILENAME)
 
 
-def does_report_data_exist():
-    return _REPORT_DATA_PATH.exists()
-
-
-def report_data_exists_marker():
-    return pytest.mark.skipif(
-        not does_report_data_exist(), reason="Report data does not exist."
-    )
-
-
 @pytest.fixture
 def report_data():
     return json.loads(_REPORT_DATA_PATH.read_text())
@@ -28,7 +18,6 @@ def report_data_fixture():
     return json.loads(_REPORT_DATA_FIXTURE.read_text())
 
 
-@report_data_exists_marker()
 def test_totals(report_data, report_data_fixture):
     for key in report_data:
         if key.endswith("_totals") or key in ["coverages", "lineage"]:
