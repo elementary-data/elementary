02a77afb0ef98ddc2664f9988d34053fa7eea376
IDoneShaveIt <idanshavit31@gmail.com>
2023-01-03 12:42:16 +0200

    removed row number calculation from latest_tests_in_the_last_chosen_days

--- elementary/monitor/dbt_project/macros/base_queries/latest_tests_in_the_last_chosen_days.sql
+++ elementary/monitor/dbt_project/macros/base_queries/latest_tests_in_the_last_chosen_days.sql
@@ -1,10 +1,11 @@
-{% macro latest_tests_in_the_last_chosen_days(days_back = none, invocation_id = none, invocations_per_test = 1) %}
+{% macro latest_tests_in_the_last_chosen_days(days_back = none, invocation_id = none) %}
     with test_results as (
         {{ elementary_internal.current_tests_run_results_query(days_back=days_back, invocation_id=invocation_id) }}
     ),
 
     test_latest_detected_at_time as (
         select
+            {# current_tests_run_results_query sets elementary_unique_id to be test_unique_id #}
             test_unique_id,
             max(detected_at) as last_detected_at
         from test_results
@@ -14,8 +15,7 @@
     tests_in_last_chosen_days as (
         select
             *,
-            {{ elementary.datediff(elementary.cast_as_timestamp('detected_at'), elementary.current_timestamp(), 'day') }} as days_diff,
-            row_number() over (partition by test_sub_type_unique_id order by detected_at desc) as row_number
+            {{ elementary.datediff(elementary.cast_as_timestamp('detected_at'), elementary.current_timestamp(), 'day') }} as days_diff
         from test_results
     )
 
@@ -28,5 +28,4 @@
         We join between the "test_unqiue_id" to the latest test run to make sure we only query the real latest test runs for each test.
     #}
     on tests.test_unique_id = latest_runs.test_unique_id and tests.detected_at = latest_runs.last_detected_at
-    where tests.row_number <= {{ invocations_per_test }}
 {% endmacro %}
