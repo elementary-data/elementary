45db8d8d8b1ea3f761eadcaccd25b3044cf2de29
Elon Gliksberg <elongliks@gmail.com>
2023-02-13 18:17:26 +0200

    Conditionally using 'result_rows' if it's defined.

--- elementary/monitor/dbt_project/macros/get_test_results.sql
+++ elementary/monitor/dbt_project/macros/get_test_results.sql
@@ -38,7 +38,8 @@
             severity,
             status,
             days_diff,
-            invocations_rank_index
+            invocations_rank_index,
+            result_rows
         from ordered_test_results
         where invocations_rank_index <= {{ invocations_per_test }}
         order by elementary_unique_id, invocations_rank_index desc
@@ -57,7 +58,7 @@
             {% if not disable_passed_test_metrics %}
                 {% do elementary_tests_allowlist_status.append('pass') %}
             {% endif %}
-            {% set test_rows_sample = elementary_internal.get_test_rows_sample(test_result_rows_agate.get(test.id), test_type, metrics_sample_limit) %}
+            {% set test_rows_sample = elementary_internal.get_test_rows_sample(test.result_rows, test_result_rows_agate.get(test.id), test_type, test.test_results_query, metrics_sample_limit) %}
             {%- if (test_type == 'dbt_test' and status in ['fail', 'warn']) or (test_type != 'dbt_test' and status in elementary_tests_allowlist_status) -%}
                 {# Dimension anomalies return multiple dimensions for the test rows sample, and needs to be handle differently. #}
                 {# Currently we show only the anomalous for all of the dimensions. #}
--- elementary/monitor/dbt_project/macros/get_test_rows_sample.sql
+++ elementary/monitor/dbt_project/macros/get_test_rows_sample.sql
@@ -1,13 +1,31 @@
-{%- macro get_test_rows_sample(result_rows_agate, test_type, results_sample_limit=5) -%}
-    {% set test_rows_sample = [] %}
+{%- macro get_test_rows_sample(result_rows, result_rows_agate, test_type, test_query, results_sample_limit=5) -%}
+    {% set should_limit_sample = test_type == 'dbt_test' %}
+
+    {% if result_rows is defined and result_rows is not none %}
+        {% set result_rows = fromjson(result_rows) %}
+        {% if should_limit_sample %}
+            {% do return(result_rows[:results_sample_limit]) %}
+        {% endif %}
+        {% do return(result_rows) %}
+    {% endif %}
+
     {% if result_rows_agate %}
+        {% set test_rows_sample = [] %}
         {% set result_row_column = result_rows_agate.columns["result_row"] %}
-        {% if test_type == 'dbt_test' %}
-          {% set result_row_column = result_row_column[:results_sample_limit] %}
+        {% if should_limit_sample %}
+            {% set result_row_column = result_row_column[:results_sample_limit] %}
         {% endif %}
         {% for result_row in result_row_column %}
-          {% do test_rows_sample.append(fromjson(result_row)) %}
+            {% do test_rows_sample.append(fromjson(result_row)) %}
         {% endfor %}
+        {{ return(test_rows_sample) }}
     {% endif %}
-    {{ return(test_rows_sample) }}
+
+  {% set query %}
+    with test_results as (
+      {{ test_query }}
+    )
+    select * from test_results {% if should_limit_sample %} limit {{ sample_limit }} {% endif %}
+  {% endset %}
+  {% do return(elementary.agate_to_dicts(elementary.run_query(query))) %}
 {%- endmacro -%}
