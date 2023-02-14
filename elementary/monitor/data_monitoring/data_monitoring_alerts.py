a8902c6cac938a0207da39b967e83a8d06891edc
IDoneShaveIt <idanshavit31@gmail.com>
2023-02-14 18:20:16 +0200

    Fixed data monitoring alerts after the merge with master

--- elementary/monitor/data_monitoring/data_monitoring_alerts.py
+++ elementary/monitor/data_monitoring/data_monitoring_alerts.py
@@ -12,8 +12,8 @@ from elementary.monitor.alerts.alerts import Alerts
 from elementary.monitor.alerts.model import ModelAlert
 from elementary.monitor.alerts.source_freshness import SourceFreshnessAlert
 from elementary.monitor.alerts.test import TestAlert
+from elementary.monitor.api.alerts.alerts import AlertsAPI
 from elementary.monitor.data_monitoring.data_monitoring import DataMonitoring
-from elementary.monitor.fetchers.alerts.alerts import AlertsFetcher
 from elementary.tracking.anonymous_tracking import AnonymousTracking
 from elementary.utils.json_utils import prettify_json_str_set
 from elementary.utils.log import get_logger
@@ -38,7 +38,7 @@ class DataMonitoringAlerts(DataMonitoring):
             config, tracking, force_update_dbt_package, disable_samples, filter
         )
 
-        self.alerts_fetcher = AlertsFetcher(
+        self.alerts_api = AlertsAPI(
             self.internal_dbt_runner,
             self.config,
             self.elementary_database_and_schema,
@@ -138,7 +138,7 @@ class DataMonitoringAlerts(DataMonitoring):
             self.execution_properties["success"] = self.success
             return self.success
 
-        alerts = self.alerts_fetcher.get_new_alerts(
+        alerts = self.alerts_api.get_new_alerts(
             days_back,
             disable_samples=self.disable_samples,
             filter=self.filter.get_filter(),
