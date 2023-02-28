c90450ccca903bb6e76adb92b2fad7142addcbe6
RoiTabach <roi.tabach@gmail.com>
2023-02-28 10:58:27 +0200

    add "update sent" and "skip alerts" to alertsAPI's mock

--- tests/mocks/api/alerts_api_mock.py
+++ tests/mocks/api/alerts_api_mock.py
@@ -1,4 +1,8 @@
+from collections import defaultdict
+from typing import Dict, Set, List, Union
 from elementary.config.config import Config
+from elementary.monitor.alerts.alerts import AlertType
+from elementary.monitor.alerts.malformed import MalformedAlert
 from elementary.monitor.api.alerts.alerts import AlertsAPI
 from tests.mocks.dbt_runner_mock import MockDbtRunner
 from tests.mocks.fetchers.alerts_fetcher_mock import MockAlertsFetcher
@@ -12,3 +16,12 @@ class MockAlertsAPI(AlertsAPI):
             mock_dbt_runner, config, elementary_database_and_schema="test.test"
         )
         self.alerts_fetcher = MockAlertsFetcher()
+        self.sent_alerts: Dict[str, Set[str]] = defaultdict(set)  # map from table_name to alert IDs
+        self.skipped_alerts: Dict[str, Set[str]] = defaultdict(set)  # map from table_name to alert IDs
+    def update_sent_alerts(self, alert_ids: List[str], table_name: str) -> None:
+        self.sent_alerts[table_name].update(alert_ids)
+
+    def skip_alerts(
+        self, alerts_to_skip: List[Union[AlertType, MalformedAlert]], table_name: str
+    ) -> None:
+        self.skipped_alerts[table_name].update([alert.id for alert in alerts_to_skip])
