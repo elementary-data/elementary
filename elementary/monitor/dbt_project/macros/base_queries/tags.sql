d4c701354e4fdc10f73f8e41bc21234507dc5a62
IDoneShaveIt <idanshavit31@gmail.com>
2023-04-23 17:41:27 +0300

    ELE-624 test configuration metadata API (for SaaS)

---
+++ elementary/monitor/dbt_project/macros/base_queries/tags.sql
@@ -0,0 +1,23 @@
+{% macro project_tags() %}
+    {% set project_tags_query %}
+        with dbt_models as (
+            select * from {{ ref('elementary', 'dbt_models') }}
+        ),
+
+        dbt_sources as (
+            select * from {{ ref('elementary', 'dbt_sources') }}
+        ),
+
+        dbt_tests as (
+            select * from {{ ref('elementary', 'dbt_tests') }}
+        )
+
+        select tags from dbt_models
+        union
+        select tags from dbt_sources
+        union
+        select tags from dbt_tests 
+    {% endset %}
+    {% set tags_agate = run_query(project_tags_query) %}
+    {% do return(elementary.agate_to_dicts(tags_agate)) %}
+{% endmacro %}
