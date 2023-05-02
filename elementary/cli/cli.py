de462f57465f447707c8e1c0ffa31670aa1e96a6
Manul Patel <manulpatel99@gmail.com>
2023-05-03 00:56:31 +0530

    Fixed breakdown of existing cli commands with --version.

--- elementary/cli/cli.py
+++ elementary/cli/cli.py
@@ -71,10 +71,10 @@ cli = ElementaryCLI(
     help="Open source data reliability solution (https://docs.elementary-data.com/)"
 )
 
-@click.group(invoke_without_command=True, no_args_is_help=True)
+@click.command(cls=ElementaryCLI)
 @click.version_option(version=package.get_package_version(), message="Elementary installed version %(version)s.")
-def cli(ctx):
-   pass
+def cli():
+    pass
 
 if __name__ == "__main__":
     cli()
