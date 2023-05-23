3dd72009628ed0d276924740322916e522bdb99f
Elon Gliksberg <elongliks@gmail.com>
2023-05-23 11:30:41 +0300

    Table materialization always uses the SQL.

--- elementary/monitor/dbt_project/macros/materializations/table.sql
+++ elementary/monitor/dbt_project/macros/materializations/table.sql
@@ -4,22 +4,16 @@
     {{ return(dbt.materialization_table_default()) }}
   {% endif %}
 
-  {% set existing_relation = load_cached_relation(this) %}
-  {% if not existing_relation %}
-    {% set init_schema_sql %}
-      with result as (
-        {{ sql }}
-      )
-      select * from result where 1 = 0
-    {% endset %}
-    {% do context.update({"sql": init_schema_sql}) %}
-  {% else %}
-    {% set same_sql %}
-      select * from {{ this }}
-    {% endset %}
-    {% do context.update({"sql": same_sql}) %}
-  {% endif %}
-
-  {% do context.update({"pre_hooks": [], "post_hooks": []}) %}
+  {% set init_schema_sql %}
+    with result as (
+      {{ sql }}
+    )
+    select * from result where 1 = 0
+  {% endset %}
+  {% do context.update({
+    "sql": init_schema_sql,
+    "pre_hooks": [],
+    "post_hooks": [],
+    }) %}
   {{ return(dbt.materialization_table_default()) }}
 {% endmaterialization %}
