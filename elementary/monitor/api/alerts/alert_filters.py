2aca24e2476cce1c5448741d3f593654abde787c
Ella Katz <ella@elementary-data.com>
2023-07-12 14:26:08 +0300

    rename variable to not override native python filter function

--- elementary/monitor/api/alerts/alert_filters.py
+++ elementary/monitor/api/alerts/alert_filters.py
@@ -14,33 +14,36 @@ logger = get_logger(__name__)
 
 def filter_alerts(
     alerts: List[AlertType],
-    filter: SelectorFilterSchema = SelectorFilterSchema(),
+    alerts_filter: SelectorFilterSchema = SelectorFilterSchema(),
 ) -> List[AlertType]:
     filtered_alerts = []
     # If the filter is empty, we want to return all of the alerts
-    if not filter.selector:
+    print("filtering alerts by:", alerts_filter)
+    if not alerts_filter.selector:
         filtered_alerts = alerts
-    elif filter.tag:
-        filtered_alerts = _filter_alerts_by_tag(alerts, filter)
-    elif filter.model:
-        filtered_alerts = _filter_alerts_by_model(alerts, filter)
-    elif filter.owner:
-        filtered_alerts = _filter_alerts_by_owner(alerts, filter)
-    elif filter.node_names is not None:
-        filtered_alerts = _filter_alerts_by_node_names(alerts, filter)
+    elif alerts_filter.tag:
+        filtered_alerts = _filter_alerts_by_tag(alerts, alerts_filter)
+    elif alerts_filter.model:
+        filtered_alerts = _filter_alerts_by_model(alerts, alerts_filter)
+    elif alerts_filter.owner:
+        filtered_alerts = _filter_alerts_by_owner(alerts, alerts_filter)
+    elif alerts_filter.status:
+        filtered_alerts = _filter_alerts_by_status(alerts, alerts_filter)
+    elif alerts_filter.node_names is not None:
+        filtered_alerts = _filter_alerts_by_node_names(alerts, alerts_filter)
     # If the filter contains a filter that we don't support, we want to return an empty list of alerts
-    elif filter.selector:
+    elif alerts_filter.selector:
         logger.error(
-            f"An unsupported alerts selector has been provided - {filter.selector}!\nNo alert has been sent!"
+            f"An unsupported alerts selector has been provided - {alerts_filter.selector}!\nNo alert has been sent!"
         )
     return filtered_alerts
 
 
 def _filter_alerts_by_tag(
     alerts: List[AlertType],
-    filter: SelectorFilterSchema,
+    tag_filter: SelectorFilterSchema,
 ) -> List[AlertType]:
-    if filter.tag is None:
+    if tag_filter.tag is None:
         return alerts
 
     filtered_alerts = []
@@ -52,16 +55,16 @@ def _filter_alerts_by_tag(
         )
         alert_tags = try_load_json(raw_tags) if isinstance(raw_tags, str) else raw_tags
 
-        if alert_tags and filter.tag in alert_tags:
+        if alert_tags and tag_filter.tag in alert_tags:
             filtered_alerts.append(alert)
     return filtered_alerts
 
 
 def _filter_alerts_by_owner(
     alerts: List[AlertType],
-    filter: SelectorFilterSchema,
+    owner_filter: SelectorFilterSchema,
 ) -> List[AlertType]:
-    if filter.owner is None:
+    if owner_filter.owner is None:
         return alerts
 
     filtered_alerts = []
@@ -75,31 +78,31 @@ def _filter_alerts_by_owner(
             try_load_json(raw_owners) if isinstance(raw_owners, str) else raw_owners
         )
 
-        if alert_owners and filter.owner in alert_owners:
+        if alert_owners and owner_filter.owner in alert_owners:
             filtered_alerts.append(alert)
     return filtered_alerts
 
 
 def _filter_alerts_by_model(
     alerts: List[AlertType],
-    filter: SelectorFilterSchema,
+    model_filter: SelectorFilterSchema,
 ) -> List[AlertType]:
-    if filter.model is None:
+    if model_filter.model is None:
         return alerts
 
     filtered_alerts: List[AlertType] = []
     for alert in alerts:
         alert_model_unique_id = alert.model_unique_id
-        if alert_model_unique_id and alert_model_unique_id.endswith(filter.model):
+        if alert_model_unique_id and alert_model_unique_id.endswith(model_filter.model):
             filtered_alerts.append(alert)
     return filtered_alerts
 
 
 def _filter_alerts_by_node_names(
     alerts: List[AlertType],
-    filter: SelectorFilterSchema,
+    node_name_filter: SelectorFilterSchema,
 ) -> List[AlertType]:
-    if filter.node_names is None:
+    if node_name_filter.node_names is None:
         return alerts
 
     filtered_alerts = []
@@ -116,7 +119,7 @@ def _filter_alerts_by_node_names(
             raise Exception(f"Unexpected alert type: {type(alert)}")
 
         if alert_node_name:
-            for node_name in filter.node_names:
+            for node_name in node_name_filter.node_names:
                 if alert_node_name.endswith(node_name) or node_name.endswith(
                     alert_node_name
                 ):
