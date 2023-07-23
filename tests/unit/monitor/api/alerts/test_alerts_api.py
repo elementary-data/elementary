5852cef4aae221fc6d32118a5dc7df4fcf61b3a2
Ella Katz <ella@elementary-data.com>
2023-07-23 15:17:03 +0300

    fix test

--- tests/unit/monitor/api/alerts/test_alerts_api.py
+++ tests/unit/monitor/api/alerts/test_alerts_api.py
@@ -21,12 +21,10 @@ def test_get_suppressed_alerts(alerts_api_mock: MockAlertsAPI):
     suppressed_test_alerts = alerts_api_mock._get_suppressed_alerts(
         test_alerts,
         last_test_alert_sent_times,
-        alerts_api_mock.global_suppression_interval,
     )
     suppressed_model_alerts = alerts_api_mock._get_suppressed_alerts(
         model_alerts,
         last_model_alert_sent_times,
-        alerts_api_mock.global_suppression_interval,
     )
 
     assert json.dumps(suppressed_test_alerts, sort_keys=True) == json.dumps(
