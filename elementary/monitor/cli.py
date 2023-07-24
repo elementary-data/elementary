d38a25ed219ca97403ac902ed9c133e9ffe9130b
Ella Katz <ella@elementary-data.com>
2023-07-24 12:42:18 +0300

    clearer name

--- elementary/monitor/cli.py
+++ elementary/monitor/cli.py
@@ -228,10 +228,11 @@ def get_cli_properties() -> dict:
     help="Whether to group alerts by 'alert' or by 'table'",
 )
 @click.option(
-    "--override-config",
+    "--override-dbt-project-config",
+    "-oc",
     is_flag=True,
-    help="Whether to override the settings (slack channel, suppression interval, etc) "
-    "in config/meta with the provided parameters.",
+    help="Whether to override the settings (slack channel, suppression interval) "
+    "in the model or test meta in the dbt project with the parameters provided by the CLI.",
 )
 @click.pass_context
 def monitor(
@@ -258,7 +259,7 @@ def monitor(
     group_by,
     target_path,
     suppression_interval,
-    override_config,
+    override_dbt_project_config,
 ):
     """
     Get alerts on failures in dbt jobs.
@@ -300,7 +301,7 @@ def monitor(
             disable_samples=disable_samples,
             filter=select,
             global_suppression_interval=suppression_interval,
-            override_config=override_config,
+            override_config=override_dbt_project_config,
         )
         # The call to track_cli_start must be after the constructor of DataMonitoringAlerts as it enriches the tracking
         # properties. This is a tech-debt that should be fixed in the future.
