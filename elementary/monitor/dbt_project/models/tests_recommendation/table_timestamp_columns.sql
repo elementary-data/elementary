8ad370e505a3b6ec30b854c3a8ad959ab436c11a
Elon Gliksberg <elongliks@gmail.com>
2023-08-17 14:09:37 +0300

    Implemented 'table_timestamp_columns' model.

--- elementary/monitor/dbt_project/models/tests_recommendation/table_timestamp_columns.sql
+++ elementary/monitor/dbt_project/models/tests_recommendation/table_timestamp_columns.sql
@@ -29,54 +29,81 @@
     "%_updated_at",
     "%_created_at",
 ] %}
-{% set timestamp_column_sql_expr_filter = "('{}')".format(
+{% set joined_timestamp_column_names = "'{}'".format(
     "', '".join(timestamp_column_names)
 ) %}
 
-{% set fqn_sql_expr = "database_name, schema_name, table_name" %}
 
 with
-    tables_criticality as (
-        select *
-        from {{ ref("tables_criticality") }}
-    ),
-
     columns as (
-        select distinct *
+        select distinct
+            lower(database_name) as database_name,
+            lower(schema_name) as schema_name,
+            lower(table_name) as table_name,
+            lower(column_name) as column_name
         from {{ ref("elementary", "dbt_columns") }}
     ),
 
-    timestamp_columns as (
-        select *
+    inferred_timestamp_columns as (
+        select
+            database_name,
+            schema_name,
+            table_name,
+            column_name,
+            timestamp_column_names.confidence
         from columns
-        where
-            lower(column_name) in {{ timestamp_column_sql_expr_filter }}
-            {% for timestamp_like_column_name in timestamp_like_column_names %}
-                or lower(column_name) like '{{ timestamp_like_column_name }}'
-            {% endfor %}
-            -- Sources in which the user already provided the the timestamp column.
-            or ({{ fqn_sql_expr }}) in (
-                select {{ fqn_sql_expr }}
-                from {{ ref("elementary", "dbt_sources") }}
-                where loaded_at_field is not null
+        join
+            (
+                values
+                    {% for timestamp_column_name in timestamp_column_names %}
+                        ('{{ timestamp_column_name }}', {{ loop.index }})
+                        {% if not loop.last %},{% endif %}
+                    {% endfor %}
+            ) as timestamp_column_names(column_name, confidence) using (column_name)
+        order by timestamp_column_names.confidence
+    ),
+
+    source_provided_timestamp_columns as (
+        select
+            lower(database_name) as database_name,
+            lower(schema_name) as schema_name,
+            lower(name) as table_name,
+            lower(loaded_at_field) as column_name
+        from {{ ref("elementary", "dbt_sources") }}
+        where loaded_at_field is not null
+    ),
+
+    rated_timestamp_columns as (
+        -- Combine inferred and source-provided timestamp columns,
+        -- giving priority to source-provided ones.
+        select
+            database_name,
+            schema_name,
+            table_name,
+            coalesce(source.column_name, inferred.column_name) as column_name,
+            case
+                when source.column_name is not null then 0 else inferred.confidence
+            end as absolute_confidence
+        from inferred_timestamp_columns inferred
+        full outer join
+            source_provided_timestamp_columns source using (
+                database_name, schema_name, table_name, column_name
             )
+    ),
+
+    best_rated_timestamp_columns as (
+        select
+            database_name,
+            schema_name,
+            table_name,
+            column_name,
+            row_number() over (
+                partition by database_name, schema_name, table_name
+                order by absolute_confidence
+            ) as relative_confidence
+        from rated_timestamp_columns
     )
 
-
-select
-    {{ fqn_sql_expr }},
-    resource_name,
-    source_name,
-    depends_on_count,
-    dependant_on_count,
-    exposure_count,
-    string_agg(column_name, ', ') as timestamp_columns
-from timestamp_columns
-join tables_criticality using ({{ fqn_sql_expr }})
-group by
-    {{ fqn_sql_expr }},
-    resource_name,
-    source_name,
-    depends_on_count,
-    dependant_on_count,
-    exposure_count
+select database_name, schema_name, table_name, column_name as timestamp_column
+from best_rated_timestamp_columns
+where relative_confidence = 1
