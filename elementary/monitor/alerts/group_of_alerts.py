e2ebe7ea146aaa2f59f147d50e65c0a24342f18c
Noy Arie <noyarie1992@gmail.com>
2023-09-04 15:34:09 +0300

    Alert links updates (#1137)
    * change copy of alert links
    
    * no link for model error - grouped
    
    * rename

--- elementary/monitor/alerts/group_of_alerts.py
+++ elementary/monitor/alerts/group_of_alerts.py
@@ -5,10 +5,7 @@ from elementary.clients.slack.schema import SlackMessageSchema
 from elementary.clients.slack.slack_message_builder import SlackMessageBuilder
 from elementary.monitor.alerts.alert import Alert, SlackAlertMessageBuilder
 from elementary.monitor.alerts.model import ModelAlert
-from elementary.monitor.alerts.report_link_utils import (
-    get_model_runs_link,
-    get_model_test_runs_link,
-)
+from elementary.monitor.alerts.report_link_utils import get_model_test_runs_link
 from elementary.monitor.alerts.schema.alert import ReportLinkData
 from elementary.monitor.alerts.schema.alert_group_component import (
     AlertGroupComponent,
@@ -352,10 +349,10 @@ class GroupOfAlertsByTable(GroupOfAlerts):
         return preview_blocks
 
     def _get_report_link(self) -> Optional[ReportLinkData]:
-        if self._components_to_alerts.get(ModelErrorComponent):
-            return get_model_runs_link(self._report_url, self._model_unique_id)
-        else:
+        # No report link when there is only model error
+        if self._components_to_alerts.get(ModelErrorComponent) is None:
             return get_model_test_runs_link(self._report_url, self._model_unique_id)
+        return None
 
 
 class GroupOfAlertsBySingleAlert(GroupOfAlerts):
