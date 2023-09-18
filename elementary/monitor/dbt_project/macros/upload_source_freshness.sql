89898e95f7d1aa3d86b214332428cb1947ec745d
Noy Arie <noy@elementary-data.com>
2023-09-18 13:28:52 +0300

    fix typo

--- elementary/monitor/dbt_project/macros/upload_source_freshness.sql
+++ elementary/monitor/dbt_project/macros/upload_source_freshness.sql
@@ -1,5 +1,5 @@
 {% macro upload_source_freshness(results) %}
-  {% set source_freshness_results_dicts = fromjson(results) %}
+  {% set source_freshness_results_dict = fromjson(results) %}
   {% set source_freshness_results_relation = ref('dbt_source_freshness_results') %}
-  {% do elementary.upload_artifacts_to_table(source_freshness_results_relation, source_freshness_results_dicts, elementary.flatten_source_freshness, append=True, should_commit=true) %}
+  {% do elementary.upload_artifacts_to_table(source_freshness_results_relation, source_freshness_results_dict, elementary.flatten_source_freshness, append=True, should_commit=true) %}
 {% endmacro %}
