ce66ee88796c3a1ed7b888b9d4672159778a4012
Noy Arie <noy@elementary-data.com>
2023-09-19 11:53:36 +0300

    remove falsy condition

--- elementary/operations/upload_source_freshness.py
+++ elementary/operations/upload_source_freshness.py
@@ -21,11 +21,10 @@ class UploadSourceFreshnessOperation:
                 "Path to dbt project is missing. Please run the command with `--project-dir <DBT_PROJECT_DIR>`."
             )
         sources_file_contents = self.get_sources_file_contents()
-        if sources_file_contents:
-            results = sources_file_contents["results"]
-            metadata = sources_file_contents["metadata"]
-            self.upload_results(results, metadata)
-            click.echo("Uploaded source freshness results successfully.")
+        results = sources_file_contents["results"]
+        metadata = sources_file_contents["metadata"]
+        self.upload_results(results, metadata)
+        click.echo("Uploaded source freshness results successfully.")
 
     def get_sources_file_contents(self) -> dict:
         source_path = self.get_target_path() / "sources.json"
