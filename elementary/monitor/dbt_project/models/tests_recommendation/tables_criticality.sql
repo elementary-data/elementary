d2c4f2ebc219630985e148fb1bda8d1af26e5486
Elon Gliksberg <elongliks@gmail.com>
2023-10-02 12:11:32 +0300

    Resolving exposures' relation.

--- elementary/monitor/dbt_project/models/tests_recommendation/tables_criticality.sql
+++ elementary/monitor/dbt_project/models/tests_recommendation/tables_criticality.sql
@@ -1,9 +1,6 @@
-{% if not execute %}
-    {% do return(none) %}
-{% endif %}
 {% set exposures_relation = ref('elementary', 'enriched_exposures') %}
 {% if not elementary.relation_exists(exposures_relation) %}
-    {% set exposures_relation = ref('elementary', 'dbt_exposures')} %}
+    {% set exposures_relation = ref('elementary', 'dbt_exposures') %}
 {% endif %}
 
 with
