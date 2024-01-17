14e7c77ef470326047d31543830fc9a08c27e1ac
Thomas Elvey <tom@field.energy>
2024-01-17 15:23:22 +0000

    Force get_adapter_type_and_unique_id to fail, therefore setting warehouse_type to null

--- elementary/monitor/dbt_project/macros/get_adapter_type_and_unique_id.sql
+++ elementary/monitor/dbt_project/macros/get_adapter_type_and_unique_id.sql
@@ -13,3 +13,7 @@
 {% macro bigquery__get_adapter_unique_id() %}
     {{ return(target.project) }}
 {% endmacro %}
+
+{% macro trino__get_adapter_unique_id() %}
+    {{ exceptions.raise_compiler_error("Force failure and thus set warehouse_type=null since Trino is not currently supported within the report index.html") }}
+{% endmacro %}
