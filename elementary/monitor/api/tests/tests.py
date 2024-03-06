e4dfc00a21484b79b50e70c980ffe45e8c3830a8
Noy Arie <noy@elementary-data.com>
2024-03-06 15:14:01 +0200

    test results and runs includes keys for singular as well

--- elementary/monitor/api/tests/tests.py
+++ elementary/monitor/api/tests/tests.py
@@ -133,7 +133,7 @@ class TestsAPI(APIClient):
         self,
         invocation_id: Optional[str],
         disable_samples: bool = False,
-    ) -> Dict[Optional[str], List[TestResultSchema]]:
+    ) -> Dict[str, List[TestResultSchema]]:
         filtered_test_results_db_rows = self.test_results_db_rows
 
         if invocation_id:
@@ -149,9 +149,7 @@ class TestsAPI(APIClient):
             if test_result.invocations_rank_index == 1
         ]
 
-        tests_results: DefaultDict[Optional[str], List[TestResultSchema]] = defaultdict(
-            list
-        )
+        tests_results: DefaultDict[str, List[TestResultSchema]] = defaultdict(list)
         for test_result_db_row in filtered_test_results_db_rows:
             metadata = self._get_test_metadata_from_test_result_db_row(
                 test_result_db_row
@@ -163,16 +161,18 @@ class TestsAPI(APIClient):
             if inner_test_results is None:
                 continue
 
-            tests_results[test_result_db_row.model_unique_id].append(
-                TestResultSchema(
-                    metadata=metadata,
-                    test_results=inner_test_results,
-                )
+            test_result = TestResultSchema(
+                metadata=metadata,
+                test_results=inner_test_results,
             )
+            if test_result_db_row.model_unique_id:
+                tests_results[test_result_db_row.model_unique_id].append(test_result)
+            if test_result_db_row.test_sub_type == "singular":
+                tests_results[test_result_db_row.test_unique_id].append(test_result)
 
         return tests_results
 
-    def get_test_runs(self) -> Dict[Optional[str], List[TestRunSchema]]:
+    def get_test_runs(self) -> Dict[str, List[TestRunSchema]]:
         tests_invocations = self._get_invocations(self.test_results_db_rows)
         latest_test_results = [
             test_result
@@ -191,7 +191,10 @@ class TestsAPI(APIClient):
                 ),
                 test_runs=test_invocations,
             )
-            test_runs[test_result_db_row.model_unique_id].append(test_run)
+            if test_result_db_row.model_unique_id:
+                test_runs[test_result_db_row.model_unique_id].append(test_run)
+            if test_result_db_row.test_sub_type == "singular":
+                test_runs[test_result_db_row.test_unique_id].append(test_run)
         return test_runs
 
     def _get_invocations(
