e52b70462e6630388b7cafa6dcd8f8030709b532
Noy Arie <noy@elementary-data.com>
2024-03-06 15:50:42 +0200

    types cleanup + fix test results totals

--- elementary/monitor/api/report/totals_utils.py
+++ elementary/monitor/api/report/totals_utils.py
@@ -1,29 +1,24 @@
 from collections import defaultdict
-from typing import Dict, List, Optional, Union
+from typing import Dict, List, Union
 
 from elementary.monitor.api.source_freshnesses.schema import (
-    SourceFreshnessMetadataSchema,
     SourceFreshnessResultSchema,
     SourceFreshnessRunSchema,
 )
-from elementary.monitor.api.tests.schema import (
-    TestMetadataSchema,
-    TestResultSchema,
-    TestRunSchema,
-)
+from elementary.monitor.api.tests.schema import TestResultSchema, TestRunSchema
 from elementary.monitor.api.totals_schema import TotalsSchema
 
 
 def get_total_test_results(
-    test_results: Dict[
-        Optional[str], List[Union[TestResultSchema, SourceFreshnessResultSchema]]
-    ],
-) -> Dict[Optional[str], TotalsSchema]:
-    test_metadatas = []
-    for test_result in test_results.values():
-        test_metadatas.extend([result.metadata for result in test_result])
+    test_results: Dict[str, List[Union[TestResultSchema, SourceFreshnessResultSchema]]],
+) -> Dict[str, TotalsSchema]:
+    totals: Dict[str, TotalsSchema] = defaultdict(TotalsSchema)
+    for key, test_result in test_results.items():
+        for result in test_result:
+            # count by the key of the tests_results
+            totals[key].add_total(result.metadata.latest_run_status)
 
-    return _calculate_test_results_totals(test_metadatas)
+    return totals
 
 
 def get_total_test_runs(
@@ -42,13 +37,4 @@ def get_total_test_runs(
             for test_invocation in test_invocations:
                 # count by the key of the tests_runs
                 totals[key].add_total(test_invocation.status)
-    return totals
-
-
-def _calculate_test_results_totals(
-    test_metadatas: List[Union[TestMetadataSchema, SourceFreshnessMetadataSchema]],
-) -> Dict[Optional[str], TotalsSchema]:
-    totals: Dict[Optional[str], TotalsSchema] = defaultdict(TotalsSchema)
-    for test in test_metadatas:
-        totals[test.model_unique_id].add_total(test.latest_run_status)
     return totals
