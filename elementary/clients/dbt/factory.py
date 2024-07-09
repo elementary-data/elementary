2f0f29a423674ecf43c1c0d1918ebcaa25b89591
Elon Gliksberg <elongliks@gmail.com>
2024-07-09 12:06:23 +0300

    Support configuration of dbt runner method.

--- elementary/clients/dbt/factory.py
+++ elementary/clients/dbt/factory.py
@@ -1,19 +1,24 @@
+import os
 from typing import Any, Dict, Optional, Type
 
 from dbt.version import __version__ as dbt_version_string
 from packaging import version
 
 from elementary.clients.dbt.command_line_dbt_runner import CommandLineDbtRunner
-from elementary.clients.dbt.subprocess_dbt_runner import SubprocessDbtRunner
 
 DBT_VERSION = version.Version(dbt_version_string)
 
 RUNNER_CLASS: Type[CommandLineDbtRunner]
-if DBT_VERSION >= version.Version("1.5.0"):
+if (
+    DBT_VERSION >= version.Version("1.5.0")
+    and os.getenv("DBT_RUNNER_METHOD") != "subprocess"
+):
     from elementary.clients.dbt.api_dbt_runner import APIDbtRunner
 
     RUNNER_CLASS = APIDbtRunner
 else:
+    from elementary.clients.dbt.subprocess_dbt_runner import SubprocessDbtRunner
+
     RUNNER_CLASS = SubprocessDbtRunner
 
 
