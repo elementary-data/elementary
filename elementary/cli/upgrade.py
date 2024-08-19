e321d0f00d593ceb9481f805a78e036a06f7301f
Paul Slater <paul.slater@starlingbank.com>
2024-08-19 09:36:16 +0100

    disable elementary version check
    checking the version is nice and all, but when running in production it just creates firewall log spam - we don't let traffic out anywhere, everything is explicitly allowed in kubernetes overlay network policy as well as an overarching firewall policy. Everytime elementary runs we see the spam trying to contact pypi to check for a newer version.
    
    This PR creates a simple config to allow disabling the check.

--- elementary/cli/upgrade.py
+++ elementary/cli/upgrade.py
@@ -3,23 +3,27 @@ import sys
 import click
 from packaging import version
 
+from elementary.config.config import Config
 from elementary.utils import package
 
 
 def recommend_version_upgrade():
-    try:
-        latest_version = package.get_latest_package_version()
-        current_version = package.get_package_version()
+    config = Config()
 
-        if not latest_version:
-            # Failed to obtain the latest version, so skip the check
-            return
+    if not config.disable_elementary_version_check:
+        try:
+            latest_version = package.get_latest_package_version()
+            current_version = package.get_package_version()
 
-        if version.parse(current_version) < version.parse(latest_version):
-            click.secho(
-                f"You are using Elementary {current_version}, however version {latest_version} is available.\n"
-                f'Consider upgrading by running: "{sys.executable} -m pip install --upgrade elementary-data"\n',
-                fg="yellow",
-            )
-    except Exception:
-        pass
+            if not latest_version:
+                # Failed to obtain the latest version, so skip the check
+                return
+
+            if version.parse(current_version) < version.parse(latest_version):
+                click.secho(
+                    f"You are using Elementary {current_version}, however version {latest_version} is available.\n"
+                    f'Consider upgrading by running: "{sys.executable} -m pip install --upgrade elementary-data"\n',
+                    fg="yellow",
+                )
+        except Exception:
+            pass
