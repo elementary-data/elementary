f5c017ee4c71431dfe5a409f62b25f3e6856b3d9
Paul Slater <paul.slater@starlingbank.com>
2024-09-02 10:12:47 +0100

    comment

--- elementary/cli/upgrade.py
+++ elementary/cli/upgrade.py
@@ -10,20 +10,22 @@ from elementary.utils import package
 def recommend_version_upgrade():
     config = Config()
 
-    if not config.disable_elementary_version_check:
-        try:
-            latest_version = package.get_latest_package_version()
-            current_version = package.get_package_version()
+    if config.disable_elementary_version_check:
+        return
 
-            if not latest_version:
-                # Failed to obtain the latest version, so skip the check
-                return
+    try:
+        latest_version = package.get_latest_package_version()
+        current_version = package.get_package_version()
 
-            if version.parse(current_version) < version.parse(latest_version):
-                click.secho(
-                    f"You are using Elementary {current_version}, however version {latest_version} is available.\n"
-                    f'Consider upgrading by running: "{sys.executable} -m pip install --upgrade elementary-data"\n',
-                    fg="yellow",
-                )
-        except Exception:
-            pass
+        if not latest_version:
+            # Failed to obtain the latest version, so skip the check
+            return
+
+        if version.parse(current_version) < version.parse(latest_version):
+            click.secho(
+                f"You are using Elementary {current_version}, however version {latest_version} is available.\n"
+                f'Consider upgrading by running: "{sys.executable} -m pip install --upgrade elementary-data"\n',
+                fg="yellow",
+            )
+    except Exception:
+        pass
