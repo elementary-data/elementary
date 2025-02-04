fa1a64ce12fb811f0859c54c1431a58b5902e28e
Ofek Weiss <ofek1weiss@gmail.com>
2025-02-04 13:47:27 +0200

    working

--- elementary/messages/formats/block_kit.py
+++ elementary/messages/formats/block_kit.py
@@ -6,6 +6,7 @@ from elementary.messages.blocks import (
     CodeBlock,
     DividerBlock,
     ExpandableBlock,
+    FactBlock,
     FactListBlock,
     HeaderBlock,
     Icon,
@@ -27,11 +28,9 @@ COLOR_MAP = {
 }
 
 
-# todo - size limits
-
-
 class BlockKitBuilder:
-    _FACT_CHUNK_SIZE = 2
+    _SECONDARY_FACT_CHUNK_SIZE = 2
+    _LONGEST_MARKDOWN_SUFFIX_LEN = 3  # length of markdown's code suffix (```)
 
     def __init__(self) -> None:
         self._blocks: list[dict] = []
@@ -64,6 +63,28 @@ class BlockKitBuilder:
             [self._format_inline_block(inline) for inline in block.inlines]
         )
 
+    def _format_markdown_section_text(self, text: str) -> dict:
+        if len(text) > slack_blocks.SectionBlock.text_max_length:
+            text = (
+                text[
+                    : slack_blocks.SectionBlock.text_max_length
+                    - len("...")
+                    - self._LONGEST_MARKDOWN_SUFFIX_LEN
+                ]
+                + "..."
+                + text[-self._LONGEST_MARKDOWN_SUFFIX_LEN :]
+            )
+        return {
+            "type": "mrkdwn",
+            "text": text,
+        }
+
+    def _format_markdown_section(self, text: str) -> dict:
+        return {
+            "type": "section",
+            "text": self._format_markdown_section_text(text),
+        }
+
     def _add_block(self, block: dict) -> None:
         if not self._is_divided:
             self._blocks.append(block)
@@ -74,15 +95,7 @@ class BlockKitBuilder:
         formatted_lines = [
             self._format_line_block_text(line_block) for line_block in block.lines
         ]
-        self._add_block(
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "\n".join(formatted_lines),
-                },
-            }
-        )
+        self._add_block(self._format_markdown_section("\n".join(formatted_lines)))
 
     def _add_header_block(self, block: HeaderBlock) -> None:
         if len(block.text) > slack_blocks.HeaderBlock.text_max_length:
@@ -100,65 +113,62 @@ class BlockKitBuilder:
         )
 
     def _add_code_block(self, block: CodeBlock) -> None:
+        self._add_block(self._format_markdown_section(f"```{block.text}```"))
+
+    def _add_primary_fact(self, fact: FactBlock) -> None:
+        self._add_block(
+            self._format_markdown_section(
+                f"*{self._format_line_block_text(fact.title)}*\n{self._format_line_block_text(fact.value)}"
+            )
+        )
+
+    def _add_secondary_facts(self, facts: List[FactBlock]) -> None:
+        if not facts:
+            return
         self._add_block(
             {
                 "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": f"```{block.text}```",
-                },
+                "fields": [
+                    self._format_markdown_section_text(
+                        f"*{self._format_line_block_text(fact.title)}*\n{self._format_line_block_text(fact.value)}"
+                    )
+                    for fact in facts
+                ],
             }
         )
 
     def _add_fact_list_block(self, block: FactListBlock) -> None:
-        if len(block.facts) == 1:
-            fact = block.facts[0]
-            self._add_block(
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": f"*{self._format_line_block_text(fact.title)}*\n{self._format_line_block_text(fact.value)}",
-                    },
-                }
-            )
-            return
-        chunked_facts = [
-            block.facts[i : i + self._FACT_CHUNK_SIZE]
-            for i in range(0, len(block.facts), self._FACT_CHUNK_SIZE)
-        ]
-        for chunk in chunked_facts:
-            self._add_block(
-                {
-                    "type": "section",
-                    "fields": [
-                        {
-                            "type": "mrkdwn",
-                            "text": f"*{self._format_line_block_text(fact.title)}*\n{self._format_line_block_text(fact.value)}",
-                        }
-                        for fact in chunk
-                    ],
-                }
-            )
+        remaining_facts = block.facts[:]
+        secondary_facts: list[FactBlock] = []
+        while remaining_facts:
+            current_fact = remaining_facts.pop(0)
+            if current_fact.primary:
+                self._add_secondary_facts(secondary_facts)
+                secondary_facts = []
+                self._add_primary_fact(current_fact)
+            else:
+                if len(secondary_facts) >= self._SECONDARY_FACT_CHUNK_SIZE:
+                    self._add_secondary_facts(secondary_facts)
+                    secondary_facts = []
+                secondary_facts.append(current_fact)
+        self._add_secondary_facts(secondary_facts)
 
     def _add_divider_block(self, block: DividerBlock) -> None:
         self._add_block({"type": "divider"})
         self._is_divided = True
 
     def _add_expandable_block(self, block: ExpandableBlock) -> None:
+        """
+        Expandable blocks are not supported in Slack Block Kit.
+        However, slack automatically collapses a large section block into an expandable block.
+        """
         self._add_block(
             {
                 "type": "section",
-                "fields": [
-                    {
-                        "type": "mrkdwn",
-                        "text": f"*{block.title}*",
-                    },
-                ],
+                "text": self._format_markdown_section_text(f"*{block.title}*"),
             }
         )
-        for sub_block in block.body:
-            self._add_message_block(sub_block)
+        self._add_message_blocks(block.body)
 
     def _add_message_block(self, block: MessageBlock) -> None:
         if isinstance(block, HeaderBlock):
@@ -180,126 +190,36 @@ class BlockKitBuilder:
         for block in blocks:
             self._add_message_block(block)
 
-    def _set_color(self, color: Color) -> None:
-        color_code = COLOR_MAP[color]
-        for attachment in self._attachment_blocks:
-            attachment["color"] = color_code
+    def _get_final_blocks(self) -> tuple[list[dict], list[dict]]:
+        """
+        Slack does not support coloring regular messages, only attachments.
+        Also, regular messages are allways displayed in full, while attachments show the first 5 blocks (with a "show more" button).
+        The way we handle this is as follows:
+        - If we have a divider block, everything up to it and including it is a regular message, and everything after it is an attachment.
+        - If we don't have a divider block, everything is an attachment (in order to always display colored messages).
+        """
+        if self._is_divided:
+            return self._blocks, self._attachment_blocks
+        else:
+            return [], self._blocks
 
     def build(self, message: MessageBody) -> Dict[str, Any]:
         self._blocks = []
         self._attachment_blocks = []
         self._add_message_blocks(message.blocks)
         color_code = COLOR_MAP.get(message.color) if message.color else None
-        if self._is_divided:
-            return {
-                "blocks": self._blocks,
-                "attachments": [
-                    {
-                        "blocks": self._attachment_blocks,
-                        "color": color_code,
-                    }
-                ],
-            }
-        else:
-            return {
-                "blocks": [],
-                "attachments": [
-                    {
-                        "blocks": self._blocks,
-                        "color": color_code,
-                    }
-                ],
-            }
+        blocks, attachment_blocks = self._get_final_blocks()
+        return {
+            "blocks": blocks,
+            "attachments": [
+                {
+                    "blocks": attachment_blocks,
+                    "color": color_code,
+                }
+            ],
+        }
 
 
 def format_block_kit(message: MessageBody) -> Dict[str, Any]:
     builder = BlockKitBuilder()
     return builder.build(message)
-
-
-# from elementary.messages.block_builders import BulletListBlock
-# from elementary.messages.blocks import (
-#     CodeBlock,
-#     DividerBlock,
-#     ExpandableBlock,
-#     FactBlock,
-#     FactListBlock,
-#     HeaderBlock,
-#     Icon,
-#     IconBlock,
-#     LineBlock,
-#     LinesBlock,
-#     LinkBlock,
-#     TextBlock,
-#     TextStyle,
-# )
-# from elementary.messages.formats.adaptive_cards import format_adaptive_card
-# from elementary.messages.message_body import Color, MessageBody
-
-# message_body = MessageBody(
-#     blocks=[
-#         HeaderBlock(text="Main Header"),
-#         LinesBlock(
-#             lines=[
-#                 LineBlock(
-#                     inlines=[
-#                         TextBlock(text="Normal text"),
-#                         TextBlock(text="Bold text", style=TextStyle.BOLD),
-#                         TextBlock(text="Italic text", style=TextStyle.ITALIC),
-#                     ]
-#                 )
-#             ]
-#         ),
-#         BulletListBlock(
-#             icon="-",
-#             lines=[
-#                 LineBlock(inlines=[TextBlock(text="First bullet point")]),
-#                 LineBlock(inlines=[TextBlock(text="Second bullet point")]),
-#             ],
-#         ),
-#         BulletListBlock(
-#             icon=Icon.CHECK,
-#             lines=[LineBlock(inlines=[TextBlock(text="Check item")])],
-#         ),
-#         FactListBlock(
-#             facts=[
-#                 FactBlock(
-#                     title=LineBlock(inlines=[TextBlock(text="Status")]),
-#                     value=LineBlock(inlines=[TextBlock(text="Passed")]),
-#                 ),
-#                 FactBlock(
-#                     title=LineBlock(inlines=[TextBlock(text="Tags")]),
-#                     value=LineBlock(inlines=[TextBlock(text="test, example")]),
-#                 ),
-#             ]
-#         ),
-#         ExpandableBlock(
-#             title="Show Details",
-#             body=[
-#                 LinesBlock(
-#                     lines=[
-#                         LineBlock(
-#                             inlines=[
-#                                 IconBlock(icon=Icon.MAGNIFYING_GLASS),
-#                                 TextBlock(text="Details Section", style=TextStyle.BOLD),
-#                             ]
-#                         ),
-#                         LineBlock(
-#                             inlines=[
-#                                 TextBlock(text="Here's some content with a"),
-#                                 LinkBlock(text="link", url="https://example.com"),
-#                             ]
-#                         ),
-#                     ]
-#                 )
-#             ],
-#             expanded=False,
-#         ),
-#     ],
-#     color=Color.RED,
-# )
-
-# d = format_block_kit(message_body)
-# import json
-
-# print(json.dumps(d, indent=2))
