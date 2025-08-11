4819dc970a85b675f773642d25345c267d5ee4de
Frank Tubbing <frank.tubbing@beequip.nl>
2025-08-11 14:22:56 +0200

    Error handling for the teams webhook alerting to handle cases like network exceptions

--- elementary/messages/messaging_integrations/teams_webhook.py
+++ elementary/messages/messaging_integrations/teams_webhook.py
@@ -69,23 +69,28 @@ class TeamsWebhookMessagingIntegration(
         body: MessageBody,
     ) -> MessageSendResult[EmptyMessageContext]:
         card = format_adaptive_card(body)
-        response = send_adaptive_card(self.url, card)
-        # The status code is not reliable for the older version and 202 for the newer version
-        # The response returns 1 in the body for the old version for success and otherwise sometext
-        # The new version is not reliable to do a check and in the response there is nothing that can be used
-        # to determine if the message was sent successfully.
-        if (
-            response.status_code not in (HTTPStatus.OK, HTTPStatus.ACCEPTED)
-            or len(response.text) > 1
-        ):
-            raise MessagingIntegrationError(
-                f"Could not post message to Teams via webhook - {self.url}. Status code: {response.status_code}, Error: {response.text}"
+        try:
+            response = send_adaptive_card(self.url, card)
+            # The status code is not reliable for the older version and 202 for the newer version
+            # The response returns 1 in the body for the old version for success and otherwise some text
+            # The new version is not reliable to do a check and in the response there is nothing that can be used
+            # to determine if the message was sent successfully.
+            if (
+                response.status_code not in (HTTPStatus.OK, HTTPStatus.ACCEPTED)
+                or len(response.text) > 1
+            ):
+                raise MessagingIntegrationError(
+                    f"Could not post message to Teams via webhook. Status code: {response.status_code}, Error: {response.text}"
+                )
+            return MessageSendResult(
+                message_context=EmptyMessageContext(),
+                timestamp=datetime.utcnow(),
+                message_format="adaptive_cards",
             )
-        return MessageSendResult(
-            message_context=EmptyMessageContext(),
-            timestamp=datetime.utcnow(),
-            message_format="adaptive_cards",
-        )
+        except requests.RequestException as e:
+            raise MessagingIntegrationError(
+                f"Network error while posting message to Teams webhook: {str(e)}"
+            ) from e
 
     def supports_reply(self) -> bool:
         return False
