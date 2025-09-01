04cababcb94b6818e812afbf117292a2103001e7
Itamar Hartstein <haritamar@gmail.com>
2025-09-01 17:20:38 +0300

    don't run deps from the runner for dbt fusion

--- elementary/clients/dbt/dbt_fusion_runner.py
+++ elementary/clients/dbt/dbt_fusion_runner.py
@@ -8,3 +8,7 @@ DBT_FUSION_PATH = os.getenv("DBT_FUSION_PATH", "~/.local/bin/dbt")
 class DbtFusionRunner(SubprocessDbtRunner):
     def _get_dbt_command_name(self) -> str:
         return os.path.expanduser(DBT_FUSION_PATH)
+
+    def _run_deps_if_needed(self):
+        # Currently we don't support auto-updating deps for dbt fusion
+        return
