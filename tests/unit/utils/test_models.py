7f5c5075861bc302f2a7e3ee3f7e1b6ab95f44e7
Ryosuke839 <ryosukem16@gmail.com>
2025-10-30 14:35:18 +0900

    Add tests for get_shortened_model_name

---
+++ tests/unit/utils/test_models.py
@@ -0,0 +1,23 @@
+import json
+
+from elementary.utils.models import get_shortened_model_name
+
+
+def test_shorten_none():
+    shortened = get_shortened_model_name(None)
+    assert shortened is None
+
+
+def test_shorten_1_part():
+    shortened = get_shortened_model_name("foo")
+    assert shortened == "foo"
+
+
+def test_shorten_3_parts():
+    shortened = get_shortened_model_name("models.foo.bar")
+    assert shortened == "bar"
+
+
+def test_shorten_1_part():
+    shortened = get_shortened_model_name("models.foo.bar.v2")
+    assert shortened == "bar.v2"
